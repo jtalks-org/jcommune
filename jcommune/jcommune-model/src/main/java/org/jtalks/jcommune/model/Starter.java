3800af89e8e0e67e936d3be1debf62590302eb8d
Temdegon <willcode4food@tut.by>
2011-04-10 19:19:30 +0300

    All depencies are resolved and it works

--- jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/Starter.java
+++ jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/Starter.java
@@ -2,7 +2,7 @@ package org.jtalks.jcommune.model;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.hibernate.dialect.MySQL5InnoDBDialect;
 /**
  *
  * @author Temdegon
