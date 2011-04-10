728f793c801c08fd28ad5ff24d26b1bf507a1183
Temdegon <willcode4food@tut.by>
2011-04-10 19:42:06 +0300

    All depencies are resolved and it works

--- jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/Starter.java
+++ jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/Starter.java
@@ -1,8 +1,9 @@
 package org.jtalks.jcommune.model;
 
+import org.jtalks.jcommune.model.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.hibernate.dialect.MySQL5InnoDBDialect;
+
 /**
  *
  * @author Temdegon
@@ -10,5 +11,7 @@ import org.hibernate.dialect.MySQL5InnoDBDialect;
 public class Starter {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("/org/jtalks/jcommune/model/entity/applicationContext-dao.xml");
+
+        
     }
 }
