f32553a78de8303a7c9ef38507045d64049bba51
Osadchuck Eugeny <slowlysnail@gmail.com>
2011-04-16 22:20:38 +0300

    fix typo in package name

--- jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/EntityService.java
+++ jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/EntityService.java
@@ -21,12 +21,24 @@
  * The JTalks Project
  * http://www.jtalks.org
  */
-package main.java.org.jtalks.jcommune.services;
+package main.java.org.jtalks.jcommune.service;
+
+import java.util.List;
+
+import org.jtalks.jcommune.model.entity.Persistent;
 
 /**
  * @author Snail
  *
  */
-public interface EntityService {
+public interface EntityService<T extends Persistent> {
+    void saveOrUpdate(T persistent);
 
+    void delete(Long id);
+
+    void delete(T persistent);
+
+    T get(Long id);
+
+    List<T> getAll();
 }
