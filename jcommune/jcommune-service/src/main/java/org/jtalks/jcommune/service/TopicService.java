61664ca8ebd8f279c8803c09f8f96ddf1601694f
Osadchuck Eugeny <slowlysnail@gmail.com>
2011-04-18 20:16:09 +0300

    add services interfaces and their implementations

---
+++ jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/TopicService.java
@@ -0,0 +1,34 @@
+/* 
+ * JTalks for uniting people
+ * Copyright (C) 2011  JavaTalks Team
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * 
+ * This file creation date: Apr 12, 2011 / 8:05:19 PM
+ * The JTalks Project
+ * http://www.jtalks.org
+ */
+package org.jtalks.jcommune.service;
+
+import org.jtalks.jcommune.model.entity.Topic;
+
+/**
+ * @author Snail
+ *
+ */
+public interface TopicService extends EntityService<Topic> {
+
+}
