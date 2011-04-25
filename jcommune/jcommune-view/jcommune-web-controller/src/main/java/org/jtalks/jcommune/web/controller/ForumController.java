4ea2d72728bcc647befca56eeaa988fa0b258afa
Christoph256 <christoph256@mail.ru>
2011-04-25 23:37:30 +0300

    jtalks

--- jcommune/jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/ForumController.java
+++ jcommune/jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/ForumController.java
@@ -1,13 +1,16 @@
 package org.jtalks.jcommune.web.controller;
 
 
+import org.jtalks.jcommune.model.entity.Topic;
 import org.jtalks.jcommune.service.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Locale;
 
 
@@ -41,6 +44,7 @@ public class ForumController {
     @Autowired
     private TopicService topicService;
 
+
     @ModelAttribute("topicsList")
     public List<Topic> populateForum() {
         return topicService.getAll();
@@ -48,9 +52,11 @@ public class ForumController {
 
     @RequestMapping(value = "/forum", method = RequestMethod.GET)
     public ModelAndView registerPage(Locale locale) {
-        ModelAndView mav = new ModelAndView("forum");         
+        ModelAndView mav = new ModelAndView("forum");
         return mav;
     }
+
+   
 
     @RequestMapping(value = "/forum", method = RequestMethod.POST)
     public ModelAndView postPage() {
