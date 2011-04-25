8a3ecbeb987ab4d3d26208786041f0ddc76cb3c0
Osadchuck Eugeny <slowlysnail@gmail.com>
2011-04-25 12:21:16 +0300

    merge fixed version

--- jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/dao/hibernate/TopicHibernateDao.java
+++ jcommune/jcommune-model/src/main/java/org/jtalks/jcommune/model/dao/hibernate/TopicHibernateDao.java
@@ -28,29 +28,41 @@ import org.hibernate.Query;
 import org.jtalks.jcommune.model.entity.Topic;
 
 /**
- * Data Access Object for {@link Topic} instances
+ * Data Access Object for {@link Topic} instances.
  * 
- * @author Temdegon
+ * @author Pavel Vervenko
  */
 public class TopicHibernateDao extends AbstractHibernateDao<Topic> {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void saveOrUpdate(Topic topic) {
         getSession().save(topic);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void delete(Long id) {
-        Query query = getSession().createQuery("delete Author where id= :topicId");
+        Query query = getSession().createQuery("delete Topic where id= :topicId");
         query.setLong("topicId", id);
         query.executeUpdate();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Topic get(Long id) {
         return (Topic) getSession().load(Topic.class, id);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Topic> getAll() {
         return getSession().createQuery("from Topic").list();
