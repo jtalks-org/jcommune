6e6f17e9380869d7c3f774029e63bc335ab0dd1a
Osadchuck Eugeny <slowlysnail@gmail.com>
2011-04-27 22:14:53 +0300

    fetch fix from TQFP64 fix fro JC12-locale

--- jcommune/jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/ForumController.java
+++ jcommune/jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/ForumController.java
@@ -1,16 +1,13 @@
 package org.jtalks.jcommune.web.controller;
 
 
-import org.jtalks.jcommune.model.entity.Topic;
 import org.jtalks.jcommune.service.TopicService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import java.util.Locale;
 
 
 /**
@@ -40,17 +37,13 @@ import java.util.List;
 
 @Controller
 public class ForumController {
-    @Autowired
+
     private TopicService topicService;
 
-    @ModelAttribute("topicsList")
-    public List<Topic> populateForum() {
-        return topicService.getAll();
-    }
-
     @RequestMapping(value = "/forum", method = RequestMethod.GET)
-    public ModelAndView registerPage() {
-        ModelAndView mav = new ModelAndView("forum");         
+    public ModelAndView registerPage(Locale locale) {
+        ModelAndView mav = new ModelAndView("forum");        
+
         return mav;
     }
 
