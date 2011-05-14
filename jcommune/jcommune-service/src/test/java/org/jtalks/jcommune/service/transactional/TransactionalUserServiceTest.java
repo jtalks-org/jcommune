5c42559c185fb6b9c281cefd70035f9cd700ee12
wedens <wedens13@yandex.ru>
2011-05-14 23:29:08 +0700

    #JC-32 implemented tests for registerUser

--- jcommune/jcommune-service/src/test/java/org/jtalks/jcommune/service/transactional/TransactionalUserServiceTest.java
+++ jcommune/jcommune-service/src/test/java/org/jtalks/jcommune/service/transactional/TransactionalUserServiceTest.java
@@ -3,6 +3,8 @@ package org.jtalks.jcommune.service.transactional;
 import org.jtalks.jcommune.model.dao.UserDao;
 import org.jtalks.jcommune.model.entity.User;
 import org.jtalks.jcommune.service.UserService;
+import org.jtalks.jcommune.service.exceptions.DuplicateException;
+import org.mockito.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,6 +15,13 @@ import static org.mockito.Mockito.*;
  * @author Kirill Afonin
  */
 public class TransactionalUserServiceTest {
+    final String USERNAME = "username";
+    final String EMAIL = "username@mail.com";
+    final String PASSWORD = "password";
+    final String FIRST_NAME = "first name";
+    final String LAST_NAME = "last name";
+
+
     private UserService userService;
     private UserDao userDao;
 
@@ -23,8 +32,8 @@ public class TransactionalUserServiceTest {
     }
 
     @Test
-    public void testGetByUsername() throws  Exception {
-        final String USERNAME = "username";
+    public void testGetByUsername() throws Exception {
+
         User user = new User();
         user.setUsername(USERNAME);
         when(userDao.getByUsername(USERNAME)).thenReturn(user);
@@ -33,5 +42,36 @@ public class TransactionalUserServiceTest {
 
         Assert.assertEquals(USERNAME, result.getUsername(), "Username not equals");
         verify(userDao, times(1)).getByUsername(USERNAME);
+    }
+
+    @Test
+    public void testRegisterUser() throws Exception {
+        when(userDao.isUserWithEmailExist(EMAIL)).thenReturn(false);
+        when(userDao.isUserWithUsernameExist(USERNAME)).thenReturn(false);
+
+        userService.registerUser(USERNAME, EMAIL, FIRST_NAME, LAST_NAME,
+                PASSWORD);
+
+        verify(userDao, times(1)).isUserWithEmailExist(EMAIL);
+        verify(userDao, times(1)).isUserWithUsernameExist(USERNAME);
+        verify(userDao, times(1)).saveOrUpdate(Matchers.<User>any());
+    }
+
+    @Test(expectedExceptions = {DuplicateException.class})
+    public void testRegisterUser_UsernameExist() throws Exception {
+        when(userDao.isUserWithEmailExist(EMAIL)).thenReturn(false);
+        when(userDao.isUserWithUsernameExist(USERNAME)).thenReturn(true);
+
+        userService.registerUser(USERNAME, EMAIL, FIRST_NAME, LAST_NAME,
+                PASSWORD);
+    }
+
+    @Test(expectedExceptions = {DuplicateException.class})
+    public void testRegisterUser_EmailExist() throws Exception {
+        when(userDao.isUserWithEmailExist(EMAIL)).thenReturn(true);
+        when(userDao.isUserWithUsernameExist(USERNAME)).thenReturn(false);
+
+        userService.registerUser(USERNAME, EMAIL, FIRST_NAME, LAST_NAME,
+                PASSWORD);
     }
 }
