bc0c2ac953bf57b1bb91e544a5aed2b9fde1dfdf
wedens <wedens13@yandex.ru>
2011-05-16 21:09:40 +0700

    #JC-33 #JC-36 created constraint annotation for checking that objects are equals

---
+++ jcommune/jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/validation/Matches.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.jcommune.web.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Constraint for checking that two properties are equals.
+ *
+ * @author Kirill Afonin
+ * @see MatchesValidator
+ */
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = MatchesValidator.class)
+@Documented
+public @interface Matches {
+    /**
+     * @return message
+     */
+    String message() default "{validation.matches}";
+
+    /**
+     * @return groups
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * @return payload
+     */
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * @return first field name
+     */
+    String field();
+
+    /**
+     * @return second field name
+     */
+    String verifyField();
+}
