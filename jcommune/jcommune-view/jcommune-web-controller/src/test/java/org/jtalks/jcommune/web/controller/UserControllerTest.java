2175c5b6354a013bd9b3f89199cff918a4329be5
wedens <wedens13@yandex.ru>
2011-05-16 22:52:34 +0700

    #JC-35 #JC-33 implemented tests for UserController

---
+++ jcommune/jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package org.jtalks.jcommune.web.controller;
+
+import org.jtalks.jcommune.service.UserService;
+import org.jtalks.jcommune.service.exceptions.DuplicateException;
+import org.jtalks.jcommune.web.dto.UserDTO;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.ModelAndViewAssert.assertAndReturnModelAttributeOfType;
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+
+/**
+ * @author Kirill Afonin
+ */
+public class UserControllerTest {
+    private UserService userService;
+    private UserController controller;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        userService = mock(UserService.class);
+        controller = new UserController(userService);
+    }
+
+    @Test
+    public void testRegistrationPage() throws Exception {
+        ModelAndView mav = controller.registrationPage();
+
+        assertViewName(mav, "registration");
+        UserDTO dto = assertAndReturnModelAttributeOfType(mav, "newUser", UserDTO.class);
+        Assert.assertNull(dto.getEmail());
+        Assert.assertNull(dto.getUsername());
+        Assert.assertNull(dto.getPassword());
+        Assert.assertNull(dto.getPasswordConfirm());
+        Assert.assertNull(dto.getLastName());
+        Assert.assertNull(dto.getFirstName());
+    }
+
+    @Test
+    public void testRegisterUser() throws Exception {
+        UserDTO dto = getUserDTO();
+        BindingResult bindingResult = new BeanPropertyBindingResult(dto, "newUser");
+
+        ModelAndView mav = controller.registerUser(dto, bindingResult);
+
+        assertViewName(mav, "redirect:/");
+        verify(userService, times(1)).registerUser("username", "mail@mail.com",
+                null, null, "password");
+    }
+
+    @Test
+    public void testRegisterUser_Duplicate() throws Exception {
+        UserDTO dto = getUserDTO();
+        BindingResult bindingResult = new BeanPropertyBindingResult(dto, "newUser");
+        doThrow(new DuplicateException()).when(userService)
+                .registerUser(anyString(), anyString(),
+                        anyString(), anyString(), anyString());
+
+        ModelAndView mav = controller.registerUser(dto, bindingResult);
+
+        assertViewName(mav, "registration");
+        verify(userService, times(1)).registerUser(anyString(), anyString(),
+                anyString(), anyString(), anyString());
+    }
+
+    private UserDTO getUserDTO() {
+        UserDTO dto = new UserDTO();
+        dto.setUsername("username");
+        dto.setEmail("mail@mail.com");
+        dto.setPassword("password");
+        dto.setPasswordConfirm("password");
+        return dto;
+    }
+}
