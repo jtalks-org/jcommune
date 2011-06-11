65ef5565b0714857d75a91c0450f16e031466ef5
Temdegon <willcode4food@tut.by>
2011-06-12 00:17:04 +0300

    #JC-84 #JC-91skeleton for service

---
+++ jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/PrivateMessageService.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.jcommune.service;
+
+import java.util.List;
+import org.jtalks.jcommune.model.entity.PrivateMessage;
+import org.jtalks.jcommune.model.entity.User;
+
+/**
+ * The interface to manipulate with private messages.
+ * 
+ * @author Pavel Vervenko
+ */
+public interface PrivateMessageService extends EntityService<PrivateMessage> {
+
+    /**
+     * Get all inbox messages for the user.
+     * @param user the user
+     * @return the list of messages
+     */
+    List<PrivateMessage> getInbox(User user);
+
+    /**
+     * Get all outgoing messages from the user.
+     * @param user the user
+     * @return the list of messages
+     */
+    List<PrivateMessage> getOutbox(User user);
+
+    /**
+     * Save the message.
+     * @param pm the message to save.
+     */
+    void sendMessage(PrivateMessage pm);
+}
---
+++ jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/transactional/TransactionalPrivateMessageService.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.jcommune.service.transactional;
+
+import java.util.List;
+import org.jtalks.jcommune.model.dao.PrivateMessageDao;
+import org.jtalks.jcommune.model.entity.PrivateMessage;
+import org.jtalks.jcommune.model.entity.User;
+import org.jtalks.jcommune.service.PrivateMessageService;
+
+/**
+ * The implementation of PrivateMessageServices.
+ * 
+ * @author Pavel Vervenko
+ */
+public class TransactionalPrivateMessageService
+        extends AbstractTransactionalEntityService<PrivateMessage, PrivateMessageDao> implements PrivateMessageService {
+
+    private final PrivateMessageDao pmDao;
+
+    /**
+     * Creates the instance of service.
+     * @param pmDao PrivateMessageDao
+     */
+    public TransactionalPrivateMessageService(PrivateMessageDao pmDao) {
+        this.pmDao = pmDao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<PrivateMessage> getInbox(User user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<PrivateMessage> getOutbox(User user) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sendMessage(PrivateMessage pm) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
