bceed6802a580b0c9db299910786b36120234937
wedens <wedens13@yandex.ru>
2011-06-26 13:15:07 +0700

    #JC-82 implemented methods for adding and removing permissions

--- jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/nontransactional/SecurityServiceImpl.java
+++ jcommune/jcommune-service/src/main/java/org/jtalks/jcommune/service/nontransactional/SecurityServiceImpl.java
@@ -17,14 +17,24 @@
  */
 package org.jtalks.jcommune.service.nontransactional;
 
+import org.jtalks.jcommune.model.dao.UserDao;
+import org.jtalks.jcommune.model.entity.Persistent;
 import org.jtalks.jcommune.model.entity.User;
 import org.jtalks.jcommune.service.SecurityContextFacade;
 import org.jtalks.jcommune.service.SecurityService;
-import org.jtalks.jcommune.service.UserService;
-import org.jtalks.jcommune.service.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
 
 /**
  * Abstract layer for Spring Security.
@@ -34,19 +44,30 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  */
 public class SecurityServiceImpl implements SecurityService {
 
-    private UserService userService;
+    private UserDao userDao;
+    private MutableAclService mutableAclService;
     private SecurityContextFacade securityContextFacade;
+    private final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    /**
+     * Constructor creates an instance of service.
+     *
+     * @param userDao               {@link org.jtalks.jcommune.model.dao.UserDao} to be injected
+     * @param securityContextFacade {@link org.jtalks.jcommune.service.SecurityContextFacade} to be injected
+     * @param mutableAclService
+     */
+    public SecurityServiceImpl(UserDao userDao, SecurityContextFacade securityContextFacade, MutableAclService mutableAclService) {
+        this.userDao = userDao;
+        this.securityContextFacade = securityContextFacade;
+        this.mutableAclService = mutableAclService;
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public User getCurrentUser() {
-        try {
-            return userService.getByUsername(getCurrentUserUsername());
-        } catch (NotFoundException ex) {
-            return null;
-        }
+        return userDao.getByUsername(getCurrentUserUsername());
     }
 
     /**
@@ -78,22 +99,75 @@ public class SecurityServiceImpl implements SecurityService {
      * {@inheritDoc}
      */
     @Override
-    public UserDetails loadUserByUsername(String username) {
+    public void addPermissionToCurrentUser(Persistent securedObject, Permission permission) {
+        addPermission(securedObject, new PrincipalSid(getCurrentUserUsername()), permission);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addPermission(Persistent securedObject, Sid recipient, Permission permission) {
+        MutableAcl acl;
+        // create identity for securedObject
+        ObjectIdentity oid = new ObjectIdentityImpl(securedObject.getClass().getCanonicalName(), securedObject.getId());
+
         try {
-            return userService.getByUsername(username);
-        } catch (NotFoundException ex) {
-            throw new UsernameNotFoundException(ex.getMessage());
+            acl = (MutableAcl) mutableAclService.readAclById(oid);
+        } catch (NotFoundException nfe) {
+            // create new Acl if not exist
+            acl = mutableAclService.createAcl(oid);
+        }
+        // add permission to acl for recipient
+        acl.insertAce(acl.getEntries().size(), permission, recipient, true);
+        mutableAclService.updateAcl(acl);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Added permission " + permission + " for Sid " + recipient + " securedObject " + securedObject);
         }
     }
 
     /**
-     * Constructor creates an instance of service.
-     *
-     * @param userService {@link UserService} to be injected
-     * @param securityContextFacade {@link SecurityContextFacade} to be injected
+     * {@inheritDoc}
      */
-    public SecurityServiceImpl(UserService userService, SecurityContextFacade securityContextFacade) {
-        this.userService = userService;
-        this.securityContextFacade = securityContextFacade;
+    @Override
+    public void deletePermission(Persistent securedObject, Sid recipient, Permission permission) {
+        // create identity for securedObject
+        ObjectIdentity oid = new ObjectIdentityImpl(securedObject.getClass().getCanonicalName(), securedObject.getId());
+        MutableAcl acl = (MutableAcl) mutableAclService.readAclById(oid);
+
+        // Remove all permissions associated with this particular recipient (string equality used to keep things simple)
+        List<AccessControlEntry> entries = acl.getEntries();
+
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getSid().equals(recipient) && entries.get(i).getPermission().equals(permission)) {
+                acl.deleteAce(i);
+            }
+        }
+
+        mutableAclService.updateAcl(acl);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Deleted securedObject " + securedObject + " ACL permissions for recipient " + recipient);
+        }
     }
+
+    @Override
+    public void addPermissionsForAdmins(Persistent securedObject) {
+        addPermission(securedObject, new GrantedAuthoritySid("ROLE_ADMIN"), BasePermission.ADMINISTRATION);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userDao.getByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+        return user;
+    }
+
+
 }
