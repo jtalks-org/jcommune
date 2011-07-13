00afa23f49ac02d9c492907dfb318e9ead8d6c5e
Christoph256 <christoph256@mail.ru>
2011-07-13 22:46:52 +0300

    #ANTARTICLE - 6 added query.sql

---
+++ antarcticle/antarcticle-model/src/test/resources/query.sql
@@ -0,0 +1,6 @@
+use jtalks;
+
+insert into user values(1,1,'chris', 'Fuko','christoph','chff@mail.ru',123456,'admin');
+insert into article_collection values(1,1,'Collection title','Collection description');
+insert into article values(1,1,'Some Article Content','Some article title',now(),1,1,1);
+insert into comment values(1,1,now(),'some coment content',1);
