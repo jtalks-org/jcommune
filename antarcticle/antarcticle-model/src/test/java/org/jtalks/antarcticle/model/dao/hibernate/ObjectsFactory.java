905d256f8d6336a30a75c7a360c86b84ee3770d2
Dmitry <dmitry404@gmail.com>
2011-07-17 10:39:09 +0300

    #ANTARCTICLE-14 Fixed import in ObjectsFactory test

--- antarcticle/antarcticle-model/src/test/java/org/jtalks/antarcticle/model/dao/hibernate/ObjectsFactory.java
+++ antarcticle/antarcticle-model/src/test/java/org/jtalks/antarcticle/model/dao/hibernate/ObjectsFactory.java
@@ -23,7 +23,7 @@ import org.joda.time.DateTime;
 import org.jtalks.antarcticle.model.entity.Article;
 import org.jtalks.antarcticle.model.entity.ArticleCollection;
 import org.jtalks.antarcticle.model.entity.Comment;
-import org.jtalks.jcommune.model.entity.User;
+import org.jtalks.common.model.entity.User;
 
 /**
  * @author Pavel Karpukhin
