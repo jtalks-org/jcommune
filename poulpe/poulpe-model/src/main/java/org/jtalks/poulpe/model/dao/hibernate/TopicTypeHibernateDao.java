6ecfc8cb3944d9c5c67f05c15b7c312ac5f4d5f4
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2011-07-23 14:41:08 +0400

    source code of classes TransactionalTopicTypeService and TopicTypeHibernateDao was formatted

--- poulpe/poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/TopicTypeHibernateDao.java
+++ poulpe/poulpe-model/src/main/java/org/jtalks/poulpe/model/dao/hibernate/TopicTypeHibernateDao.java
@@ -7,13 +7,13 @@ import org.jtalks.poulpe.model.entity.TopicType;
 /**
  * @author Vladimir Bukhtoyarov
  */
-public class TopicTypeHibernateDao extends AbstractHibernateDao<TopicType> implements
-		org.jtalks.poulpe.model.dao.TopicTypeDao {
+public class TopicTypeHibernateDao extends AbstractHibernateDao<TopicType>
+        implements org.jtalks.poulpe.model.dao.TopicTypeDao {
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<TopicType> getAll() {
-		return getSession().createQuery("from TopicType").list();
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<TopicType> getAll() {
+        return getSession().createQuery("from TopicType").list();
+    }
 
 }
--- poulpe/poulpe-service/src/main/java/org/jtalks/poulpe/service/transactional/TransactionalTopicTypeService.java
+++ poulpe/poulpe-service/src/main/java/org/jtalks/poulpe/service/transactional/TransactionalTopicTypeService.java
@@ -7,21 +7,20 @@ import org.jtalks.poulpe.model.entity.TopicType;
 import org.jtalks.poulpe.service.TopicTypeService;
 
 public class TransactionalTopicTypeService extends
-		AbstractTransactionalEntityService<TopicType, TopicTypeDao> implements TopicTypeService {
+        AbstractTransactionalEntityService<TopicType, TopicTypeDao> implements TopicTypeService {
 
-	@Override
-	public List<TopicType> getAll() {
-		return dao.getAll();
-	}
+    @Override
+    public List<TopicType> getAll() {
+        return dao.getAll();
+    }
 
-	@Override
-	public void deleteTopicType(TopicType topicType) {
-		dao.delete(topicType.getId());
-	}
+    @Override
+    public void deleteTopicType(TopicType topicType) {
+        dao.delete(topicType.getId());
+    }
 
-	@Override
-	public void saveTopicType(TopicType topicType) {
-		dao.saveOrUpdate(topicType);
-	}
-
+    @Override
+    public void saveTopicType(TopicType topicType) {
+        dao.saveOrUpdate(topicType);
+    }
 }
