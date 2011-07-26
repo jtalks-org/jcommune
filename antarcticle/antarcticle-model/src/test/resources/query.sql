39911a26abc25efe58177c13031035db764d13ac
Dmitry <dmitry404@gmail.com>
2011-07-26 23:28:46 +0300

    Cherry-picked query.sql

--- antarcticle/antarcticle-model/src/test/resources/query.sql
+++ antarcticle/antarcticle-model/src/test/resources/query.sql
@@ -1,7 +1,10 @@
-use jtalks;
+use p_antarcticle;
 
-insert into user values(1,1,'chris', 'Fuko','christoph','chff@mail.ru',123456,'admin');
-insert into article_collection values(1,1,'Collection title','Collection description',null);
-insert into article values(1,1,'Some Article Content','Some article title',now(),1,1,1);
-update ARTICLE_COLLECTION set LAST_ARTICLE_ID=1 where ARTICLE_COLLECTION_ID=1;
-insert into comment values(1,1,now(),'some coment content',1);
+INSERT INTO `USER` VALUES(1, 1, 'chris', 'Fuko', 'christoph', 'chff@mail.ru', 123456, 'admin');
+INSERT INTO ARTICLE_COLLECTION VALUES(1, 1, 'Collection title', 'Collection description', null);
+INSERT INTO ARTICLE VALUES(1, 1, 'This is a fake article! Here should be a article content! This is a fake article! Here should be a article content! This is a fake article! Here should be a article content! This is a fake article! Here should be a article content! This is a fake article! Here should be a article content!', 'Some article title', NOW(), 1, 1, 1);
+
+UPDATE ARTICLE_COLLECTION SET LAST_ARTICLE_ID = 1 WHERE ARTICLE_COLLECTION_ID = 1;
+
+INSERT INTO COMMENT VALUES(1, 1, NOW(), 'The article is not usefull. It did not help me at all!!!', 1, 1);
+INSERT INTO COMMENT VALUES(2, 2, NOW(), 'This is a very godd article. It helped me very much! Thanks a lot!', 1, 1);
