9597fa3ae08624d5346b18787b1e2aee658ba190
senleft <alexandreteterin@gmail.com>
2011-08-24 03:32:30 +0400

    #JC-114 Fixed Sonar violation

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/dto/Breadcrumb.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/dto/Breadcrumb.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
 package org.jtalks.jcommune.web.dto;
 
 /**
@@ -9,7 +26,6 @@ package org.jtalks.jcommune.web.dto;
 public class Breadcrumb {
 
     public static final String ROOT_BREADCRUMB_LOCATION_VALUE = "Forum";
-    public static final String PM_BREADCRUMB_LOCATION_VALUE = "PM";
     public static final String INBOX_BREADCRUMB_LOCATION_VALUE = "Inbox";
     public static final String OUTBOX_BREADCRUMB_LOCATION_VALUE = "Outbox";
     public static final String DRAFTS_BREADCRUMB_LOCATION_VALUE = "Drafts";
@@ -69,10 +85,6 @@ public class Breadcrumb {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public BreadcrumbLocation getBreadcrumbLocation() {
         return breadcrumbLocation;
     }
@@ -91,14 +103,24 @@ public class Breadcrumb {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Breadcrumb that = (Breadcrumb) o;
 
-        if (breadcrumbLocation != that.breadcrumbLocation) return false;
-        if (!breadcrumbLocationValue.equals(that.breadcrumbLocationValue)) return false;
-        if (!id.equals(that.id)) return false;
+        if (breadcrumbLocation != that.breadcrumbLocation) {
+            return false;
+        }
+        if (!breadcrumbLocationValue.equals(that.breadcrumbLocationValue)) {
+            return false;
+        }
+        if (!id.equals(that.id)) {
+            return false;
+        }
 
         return true;
     }
--- jcommune-view/jcommune-web-view/src/main/webapp/WEB-INF/jsp/postForm.jsp
+++ jcommune-view/jcommune-web-view/src/main/webapp/WEB-INF/jsp/postForm.jsp
@@ -14,7 +14,7 @@
             <form:textarea path="bodyText"/>
             <form:errors path="bodyText" cols="30" rows="10"/>
             <br />
-            <a href="${pageContext.request.contextPath}/branch/${branchId}/topic/${topicId}.html" class="coolbutton" ><spring:message code='label.back'/></a>
+            <a href="${pageContext.request.contextPath}/topic/${topicId}.html" class="coolbutton" ><spring:message code='label.back'/></a>
             <button type="submit" class="coolbutton"><spring:message code='label.save'/></button>
         </div>
     </jtalks:form>
