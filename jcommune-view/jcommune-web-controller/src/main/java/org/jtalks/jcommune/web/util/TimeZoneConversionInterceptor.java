d6534c9d3666794c94ed46b08e17930cfee0177c
unknown <Naumenko@.netcracker.com>
2011-09-11 20:24:25 +0900

    =#JC-176 Time synchronization

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/util/TimeZoneConversionInterceptor.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/util/TimeZoneConversionInterceptor.java
@@ -22,31 +22,78 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 
 /**
+ * This interceptor is used to convert user's timezone id obtained from
+ * client side to canonical id. It was designed to keep conversion logic out from JSP page.
+ * <p/>
+ * If there are any processing error no exception is thrown, host server
+ * timezone is used instead.
  *
+ * @author Evgeniy Naumenko
  */
 public class TimeZoneConversionInterceptor extends HandlerInterceptorAdapter {
 
-    private static final String GMT_PARAM_NAME = "GMT";
+    public static final String GMT_PARAM_NAME = "GMT";
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getSession().getAttribute(GMT_PARAM_NAME)!= null) {
+        if (request.getSession().getAttribute(GMT_PARAM_NAME) == null) {
+            // default value will be overwritten if found
+            this.setGmtAttribute(request, this.getDefaultTimezoneId());
             for (Cookie cookie : request.getCookies()) {
                 if (cookie.getName().equals(GMT_PARAM_NAME)) {
-                    int gmt = -Integer.parseInt(cookie.getValue()) / 60;
-                    String customTimeZoneId = GMT_PARAM_NAME + (gmt > 0 ? "+" : "") + gmt;
-                    TimeZone timeZone = TimeZone.getTimeZone(customTimeZoneId);
-                    String canonicalTimeZoneId = TimeZone.getAvailableIDs(timeZone.getRawOffset())[0];
-                    request.getSession().setAttribute(GMT_PARAM_NAME, canonicalTimeZoneId);
+                    String convertedId = convertTimeZoneId(cookie.getValue());
+                    this.setGmtAttribute(request, convertedId);
+                    break;
                 }
             }
         }
         return super.preHandle(request, response, handler);
+    }
+
+    /**
+     * Converts timezone offset representation to canonical timezone id.
+     * If timezone offset representation is incorrect, then host server
+     * timezone value is used.
+     *
+     * @param jsRepresentation time difference between GMT and
+     *                         local time, in minutes. Example: "-120"
+     * @return canonical timezone id for Joda Time. Example: "America/Los_Angeles"
+     */
+    private String convertTimeZoneId(String jsRepresentation) {
+        try {
+            int gmt = -Integer.parseInt(jsRepresentation) / 60;
+            String customTimeZoneId = GMT_PARAM_NAME + this.getSignedIntString(gmt);
+            TimeZone timeZone = TimeZone.getTimeZone(customTimeZoneId);
+            /*
+            The same GMT offset may be mapped on the different canonical ids.
+            We don't care which one is used until it is mapped on the right GMT offset.
+            */
+            return TimeZone.getAvailableIDs(timeZone.getRawOffset())[0];
+        } catch (NumberFormatException e) {
+            // someone has passed wrong GMT in cookie
+            return this.getDefaultTimezoneId();
+        }
+    }
+
+    private void setGmtAttribute(HttpServletRequest request, String value) {
+        request.getSession().setAttribute(GMT_PARAM_NAME, value);
+    }
+
+    private String getDefaultTimezoneId() {
+        return TimeZone.getDefault().getID();
+    }
+
+    private String getSignedIntString(int value) {
+        if (value > 0) {
+            return "+" + value;
+        } else {
+            return String.valueOf(value);
+        }
     }
 }
