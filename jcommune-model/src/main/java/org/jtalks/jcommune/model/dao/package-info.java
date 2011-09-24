fe135965c5706c1b75f3e8d0dd8b2c4482b632d3
wedens <wedens13@yandex.ru>
2011-09-25 01:04:43 +0700

    changed license

--- jcommune-model/src/main/java/org/jtalks/jcommune/model/dao/package-info.java
+++ jcommune-model/src/main/java/org/jtalks/jcommune/model/dao/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011  jtalks.org Team
+ * Copyright (C) 2011  JTalks.org Team
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -11,11 +11,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * Also add information on how to contact you by electronic and paper mail.
- * Creation date: Apr 12, 2011 / 8:05:19 PM
- * The jtalks.org Project
  */
-
 /**
  *
  * Package for classes that mess with database. Contains DAO classes and persistent classes.
