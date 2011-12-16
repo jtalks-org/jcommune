177136d695aefed08cfe3520d2bca0fb5f2fe223
Andrey Kluev <User@.(none)>
2011-12-16 10:53:30 +0300

    # JC-282 - minor fix

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/util/Pagination.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/util/Pagination.java
@@ -14,6 +14,7 @@
  */
 package org.jtalks.jcommune.web.util;
 
+import org.jtalks.jcommune.model.entity.Post;
 import org.jtalks.jcommune.model.entity.User;
 
 import java.util.List;
@@ -172,5 +173,19 @@ public class Pagination {
      */
     public static int getPageSizeFor(User user) {
         return (user == null) ? User.DEFAULT_PAGE_SIZE : user.getPageSize();
+    }
+
+    /**
+     * Specifies the page number on which the post
+     *
+     * @param post post
+     * @return page number on which the post
+     */
+    public int definitionPostInTopic(Post post) {
+        int number = post.getTopic().getPosts().indexOf(post);
+        int res;
+        for (res = 1; res * pageSize <= number; res++) {
+        }
+        return res;
     }
 }
