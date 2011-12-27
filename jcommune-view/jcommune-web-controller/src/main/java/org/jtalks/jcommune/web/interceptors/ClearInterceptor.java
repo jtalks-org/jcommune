935eaf5faeac5f11c28707010af31db5362d8458
Andrey Kluev <elishtar@mail.ru>
2011-12-27 11:37:12 +0400

    #JC-320 Who's browsing the topic

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/interceptors/ClearInterceptor.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/interceptors/ClearInterceptor.java
@@ -16,8 +16,6 @@
 package org.jtalks.jcommune.web.interceptors;
 
 import org.jtalks.jcommune.service.nontransactional.LocationServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
