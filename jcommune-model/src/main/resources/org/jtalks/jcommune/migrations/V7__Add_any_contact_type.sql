23cee0da5ec8f5fd083b5da8cbafbb47ea3a4d2e
Andrey Kluev <User@.(none)>
2012-01-27 08:10:11 +0300

    JC-409 User Contacts:Server side. Add migration popular contacts in table CONTACT_TYPE.

---
+++ jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V7__Add_any_contact_type.sql
@@ -0,0 +1,14 @@
+INSERT INTO `CONTACT_TYPE` VALUES(1,'ICQ','/resources/images/iconOfUserContact/icq.png');
+INSERT INTO `CONTACT_TYPE` VALUES(2,'Skype','/resources/images/iconOfUserContact/skype.png');
+INSERT INTO `CONTACT_TYPE` VALUES(3,'Mail','/resources/images/iconOfUserContact/mail.png');
+INSERT INTO `CONTACT_TYPE` VALUES(4,'Telephone','/resources/images/iconOfUserContact/telephone.png');
+INSERT INTO `CONTACT_TYPE` VALUES(5,'Jabber','/resources/images/iconOfUserContact/jabber.png');
+INSERT INTO `CONTACT_TYPE` VALUES(6,'Facebook','/resources/images/iconOfUserContact/facebook.png');
+INSERT INTO `CONTACT_TYPE` VALUES(7,'Twitter','/resources/images/iconOfUserContact/twitter.png');
+INSERT INTO `CONTACT_TYPE` VALUES(8,'Google+','/resources/images/iconOfUserContact/google+.png');
+INSERT INTO `CONTACT_TYPE` VALUES(9,'Vkontakte','/resources/images/iconOfUserContact/vkontakte.png');
+INSERT INTO `CONTACT_TYPE` VALUES(10,'Odnoklassniki','/resources/images/iconOfUserContact/odnoklassniki.png');
+INSERT INTO `CONTACT_TYPE` VALUES(11,'Livejournal','/resources/images/iconOfUserContact/livejournal.png');
+INSERT INTO `CONTACT_TYPE` VALUES(12,'Tumblr','/resources/images/iconOfUserContact/tumblr.png');
+INSERT INTO `CONTACT_TYPE` VALUES(13,'Blogger','/resources/images/iconOfUserContact/blogger.png');
+
