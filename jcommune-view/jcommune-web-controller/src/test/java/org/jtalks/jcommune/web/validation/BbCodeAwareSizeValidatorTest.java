1fa952dbd4fba7aa2c9b3df5a4f1c5b45613e2c0
jk1 <eugenbox@gmail.com>
2012-03-02 12:18:14 +0400

    #JC-552 BB-code aware size validation

--- jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/BbCodeAwareSizeValidatorTest.java
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/BbCodeAwareSizeValidatorTest.java
@@ -12,7 +12,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package org.jtalks.jcommune.web.validation.validators;
+package org.jtalks.jcommune.web.validation;
 
 import org.testng.annotations.Test;
 
