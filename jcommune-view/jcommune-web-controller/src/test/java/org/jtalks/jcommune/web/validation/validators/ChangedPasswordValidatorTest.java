ab301b5a2a0253ec9c2c2a806939f4fc96aeec94
shogun <sshogunn@gmail.com>
2012-03-08 18:02:27 +0600

    #JC-578 Improve test coverage up to 95%. Move the tests in the correct packages.

--- jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/validators/ChangedPasswordValidatorTest.java
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/validators/ChangedPasswordValidatorTest.java
@@ -12,7 +12,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package org.jtalks.jcommune.web.validation.validator;
+package org.jtalks.jcommune.web.validation.validators;
 
 
 import javax.validation.ConstraintValidatorContext;
