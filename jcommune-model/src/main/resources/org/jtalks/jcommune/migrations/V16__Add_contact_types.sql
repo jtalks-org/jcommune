920b0aa829d56e9d4aec0df6ad63eaf5d889f23f
Vyacheslav Mishcheryakov <lamao@yandex.ru>
2012-04-03 14:09:19 +0300

    #JC-622 Add some new contact types.

---
+++ jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V16__Add_contact_types.sql
@@ -0,0 +1,4 @@
+INSERT INTO `CONTACT_TYPE` (`TYPE_ID`, `TYPE`, `ICON`, `UUID`) VALUES(14, 'AIM','/resources/images/iconOfUserContact/aim.png', uuid());
+INSERT INTO `CONTACT_TYPE` (`TYPE_ID`, `TYPE`, `ICON`, `UUID`) VALUES(15, 'YIM','/resources/images/iconOfUserContact/yim.png', uuid());
+INSERT INTO `CONTACT_TYPE` (`TYPE_ID`, `TYPE`, `ICON`, `UUID`) VALUES(16, 'MSN','/resources/images/iconOfUserContact/msn.png', uuid());
+INSERT INTO `CONTACT_TYPE` (`TYPE_ID`, `TYPE`, `ICON`, `UUID`) VALUES(17, 'Website','/resources/images/iconOfUserContact/site.png', uuid());
