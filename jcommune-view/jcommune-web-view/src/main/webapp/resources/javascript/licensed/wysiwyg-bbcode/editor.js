798da7b2089dbb90a0544833aab869101cdf451a
jk1 <eugenbox@gmail.com>
2012-04-15 22:42:16 +0400

    #JC-644 Cancel button for URL popup in bb editor

--- jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/licensed/wysiwyg-bbcode/editor.js
+++ jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/licensed/wysiwyg-bbcode/editor.js
@@ -344,15 +344,17 @@ function doLink() {
             createFormRow($labelUrl, "", "urlId", $labelUrlRequired) +
             '</ul>';
         $.prompt(content,
-            {buttons:{OK:true}, focus:0,
-                submit:function () {
-                    mylink = document.getElementById("urlAltId").value;
-                    var link = document.getElementById("urlId").value;
-                    if ((link != null) && (link != "")) {
-                        if (mylink == null || mylink == "") {
-                            mylink = link;
+            {buttons:{OK:true, Cancel:false}, focus:0,
+                submit:function (value, message, form) {
+                    if (value != undefined && value) {
+                        mylink = document.getElementById("urlAltId").value;
+                        var link = document.getElementById("urlId").value;
+                        if ((link != null) && (link != "")) {
+                            if (mylink == null || mylink == "") {
+                                mylink = link;
+                            }
+                            AddTag('[url=' + link + ']', '[/url]');
                         }
-                        AddTag('[url=' + link + ']', '[/url]');
                     }
                 }});
     }
