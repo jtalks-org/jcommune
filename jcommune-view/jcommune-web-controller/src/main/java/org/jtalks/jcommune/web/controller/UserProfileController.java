8bb878eb3572d09bd24986c5325a324b12f63cc5
Christoph <christoph256@mail.ru>
2012-04-27 23:02:08 +0300

    #JC-545 refactoring in UserProfileController.java

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/UserProfileController.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/UserProfileController.java
@@ -112,11 +112,7 @@ public class UserProfileController {
         //The {encodedUsername} from the JSP view automatically converted to username.
         // That's why the getByUsername() method is used instead of getByEncodedUsername().
         JCUser user = userService.getByUsername(username);
-        return new ModelAndView("userDetails")
-                .addObject("user", user)
-                        // bind separately to get localized value
-                .addObject("language", user.getLanguage())
-                .addObject("pageSize", Pagination.getPageSizeFor(user));
+        return getUserProfileModelAndView(user);
     }
 
     /**
@@ -130,8 +126,7 @@ public class UserProfileController {
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public ModelAndView showProfilePage() throws NotFoundException {
         JCUser user = securityService.getCurrentUser();
-        ModelAndView profileModelAndView = showProfilePage(user.getUsername());
-        return profileModelAndView;
+        return getUserProfileModelAndView(user);
     }
 
     /**
@@ -201,5 +196,20 @@ public class UserProfileController {
                 .addObject("pag", pag)
                 .addObject("posts", posts)
                 .addObject(BREADCRUMB_LIST, breadcrumbBuilder.getForumBreadcrumb());
+    }
+
+
+    /**
+     * Formats model and view for representing user's details
+     *
+     * @param user  user
+     * @return user's details
+     */
+    private ModelAndView getUserProfileModelAndView(JCUser user){
+        return new ModelAndView("userDetails")
+                .addObject("user", user)
+                        // bind separately to get localized value
+                .addObject("language", user.getLanguage())
+                .addObject("pageSize", Pagination.getPageSizeFor(user)); 
     }
 }
