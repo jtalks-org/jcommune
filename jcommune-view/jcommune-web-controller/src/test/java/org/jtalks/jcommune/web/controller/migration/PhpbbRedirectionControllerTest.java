14d04a622139564213ae3fe64cf9055c22e70958
jk1 <eugenbox@gmail.com>
2012-05-25 14:37:43 +0400

    #JC-706 documentation and tests

---
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/controller/migration/PhpbbRedirectionControllerTest.java
@@ -0,0 +1,21 @@
+package org.jtalks.jcommune.web.controller.migration;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PhpbbRedirectionControllerTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testShowTopic() throws Exception {
+
+    }
+
+    @Test
+    public void testShowPost() throws Exception {
+
+    }
+}
