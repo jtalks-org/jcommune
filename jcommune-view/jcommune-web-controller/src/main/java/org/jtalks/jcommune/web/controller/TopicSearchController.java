f27761d487e3a17fa4b6dd7465d2e60e3cb47942
shogun <sshogunn@gmail.com>
2012-08-22 21:06:54 +0600

    #JC-620 ResponceCode was changed to NO_CONTENT.

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/TopicSearchController.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/TopicSearchController.java
@@ -18,11 +18,13 @@ import org.jtalks.jcommune.model.entity.Topic;
 import org.jtalks.jcommune.service.TopicFullSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -69,6 +71,7 @@ public class TopicSearchController {
      * This method performs a indexing the data from the database.
      */
     @RequestMapping(value = "/search/index/rebuild")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void rebuildIndexes() {
         topicSearchService.rebuildIndex();
     }
