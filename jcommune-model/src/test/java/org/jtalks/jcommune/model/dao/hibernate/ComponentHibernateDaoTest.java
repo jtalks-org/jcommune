2095d5199c9722e5f622aae0ad19a0d37319867b
masyan <jmasyan@gmail.com>
2012-09-12 20:04:38 +0600

    for review

--- jcommune-model/src/test/java/org/jtalks/jcommune/model/dao/hibernate/ComponentHibernateDaoTest.java
+++ jcommune-model/src/test/java/org/jtalks/jcommune/model/dao/hibernate/ComponentHibernateDaoTest.java
@@ -1,12 +1,24 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.jtalks.jcommune.model.dao.hibernate;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 import org.jtalks.common.model.entity.Component;
 import org.jtalks.common.model.entity.ComponentType;
-import org.jtalks.common.model.entity.Property;
 import org.jtalks.jcommune.model.dao.ComponentDao;
-import org.jtalks.jcommune.model.dao.PropertyDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
