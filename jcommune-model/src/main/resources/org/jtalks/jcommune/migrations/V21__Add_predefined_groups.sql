131d75c10f115c9bd0d206f60ba5faf24cb003f4
Elle <lepaevaes@mail.ru>
2012-09-27 21:49:30 +0400

    #JC-932 updated migration.

--- jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V21__Add_predefined_groups.sql
+++ jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V21__Add_predefined_groups.sql
@@ -1,3 +1,11 @@
-INSERT INTO `GROUPS`(`GROUP_ID`, `UUID`, `NAME`, `DESCRIPTION`) VALUES(11, uuid(), 'Registered Users', 'The group for all registered users');
-INSERT INTO `GROUPS`(`GROUP_ID`, `UUID`, `NAME`, `DESCRIPTION`) VALUES(12, uuid(), 'Banned Users', 'The group for banned users');
-INSERT INTO `GROUPS`(`GROUP_ID`, `UUID`, `NAME`, `DESCRIPTION`) VALUES(13, uuid(), 'Administrators', 'The group for administrators of forum');
+INSERT INTO GROUPS (`UUID`, `NAME`, `DESCRIPTION`)
+  SELECT (SELECT UUID() FROM dual),'Administrators', 'Administrators group.' FROM dual
+    WHERE NOT EXISTS (SELECT gr.GROUP_ID FROM GROUPS gr WHERE gr.NAME='Administrators');
+
+INSERT INTO GROUPS (`UUID`, `NAME`, `DESCRIPTION`)
+  SELECT (SELECT UUID() FROM dual),'Registered Users', 'The group for all registered users.' FROM dual
+    WHERE NOT EXISTS (SELECT gr.GROUP_ID FROM GROUPS gr WHERE gr.NAME='Registered Users');
+
+INSERT INTO GROUPS (`UUID`, `NAME`, `DESCRIPTION`)
+  SELECT (SELECT UUID() FROM dual),'Banned Users', 'The group for banned users.' FROM dual
+    WHERE NOT EXISTS (SELECT gr.GROUP_ID FROM GROUPS gr WHERE gr.NAME='Banned Users');
