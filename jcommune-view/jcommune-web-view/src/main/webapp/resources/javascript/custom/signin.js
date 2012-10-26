3e149ae688bb421dd79dbee28701a6a5c33a5299
masyan <jmasyan@gmail.com>
2012-10-26 21:23:49 +0600

    for debug

--- jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/custom/signin.js
+++ jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/custom/signin.js
@@ -20,24 +20,24 @@
 $(function () {
 
     // hide dialog on backdrop click
-    $('.modal-backdrop').live('click', function(e) {
+    $('.modal-backdrop').live('click', function (e) {
         $('#signin-dialog').modal('hide');
     });
 
     // send ajax-request on submit button click
-    $("#signin-submit-button").live('click', function(e){
+    $("#signin-submit-button").live('click', function (e) {
         sendLoginPost();
     });
 
     // submit on press enter
-    $("#j_username, #j_password, input[name=_spring_security_remember_me]").live("keyup", function(event) {
+    $("#j_username, #j_password, input[name=_spring_security_remember_me]").live("keyup", function (event) {
         if (event.keyCode == 13) {
             sendLoginPost();
         }
     });
 
     // remove dialog from DOM on hide
-    $("#signin-dialog").live("hide", function(e) {
+    $("#signin-dialog").live("hide", function (e) {
         $("#signin-dialog").remove();
     });
 
@@ -49,10 +49,10 @@ $(function () {
 
     function signinDialog() {
         $.ajax({
-            type: "GET",
-            url: $root + "/login",
-            dataType: "html",
-            success: function (data) {
+            type:"GET",
+            url:$root + "/login",
+            dataType:"html",
+            success:function (data) {
                 var dataJq = $(data);
                 if (dataJq.find("legend").html() != null) {
                     showSigninDialog(dataJq);
@@ -78,18 +78,19 @@ function sendLoginPost() {
     var remember_me = dialog.find('input[name=_spring_security_remember_me]').attr('disabled', true).is(':checked');
     var username = dialog.find('#j_username').attr('disabled', true).val();
     var password = dialog.find('#j_password').attr('disabled', true).val();
-    
+
     var query = "j_username=" + encodeURIComponent(username) + "&" + "j_password=" + encodeURIComponent(password);
     if (remember_me) {
         query = query + "&_spring_security_remember_me=on";
     }
-
+    console.log("before " + $.now());
     $.ajax({
-        type: "POST",
-        url: $root + "/j_spring_security_check",
-        data: query,
-        dataType: "html",
-        success: function(data) {
+        type:"POST",
+        url:$root + "/j_spring_security_check",
+        data:query,
+        dataType:"html",
+        success:function (data) {
+            console.log("after bif " + $.now());
             var dataJq = $(data);
             //Check the query answer and displays prompt
             if (dataJq.find("legend").html() != null) { // signin failure
@@ -98,12 +99,14 @@ function sendLoginPost() {
                 // show new dialog with errors
                 dialog = showSigninDialog(dataJq);
                 dialog.find('#j_username').val(username);
-                dialog.find('input[name=_spring_security_remember_me]').attr('checked', remember_me);               
-            } else { // signin success
+                dialog.find('input[name=_spring_security_remember_me]').attr('checked', remember_me);
+            }
+            else { // signin success
+                console.log("after aif " + $.now());
                 location.reload();
             }
         },
-        error: function(data) {
+        error:function (data) {
             $.prompt($labelError500Detail);
         }
     });
@@ -124,15 +127,17 @@ function getFormElements(data) {
             controlLabel.remove();
             // set label text as text field placeholder
             value.find('input').attr("placeholder", text);
-        } else if (index == 3) { // checkbox
-            var checkboxCss = { 'float' : 'left', 'margin-right' : '10px' }
+        }
+        else if (index == 3) { // checkbox
+            var checkboxCss = { 'float':'left', 'margin-right':'10px' }
             value.css(checkboxCss);
         }
         ErrorUtils.addErrorStyles(value.find('span.help-inline'));
         formElements[index] = value.html();
     });
     return formElements;
-};
+}
+;
 
 /**
  * Create form element using data from server.
@@ -153,12 +158,12 @@ function composeForm(data) {
                 <button type="button" class="close" style="padding: 0; cursor: pointer; background: transparent; border: 0; -webkit-appearance: none;" data-dismiss="modal" aria-hidden="true">&times;</button> \
                 <h3>' + legendText + '</h3> \
             </div> \
-            <div class="modal-body">' 
-                  + usernameDiv
-                  + passwordDiv
-                  + rememberMeDiv + 
-                '<div class="clearfix"> \
-                    <a href="' + restorePasswordUrl + '">' + restorePasswordText + '</a> \
+            <div class="modal-body">'
+            + usernameDiv
+            + passwordDiv
+            + rememberMeDiv +
+            '<div class="clearfix"> \
+           <a href="' + restorePasswordUrl + '">' + restorePasswordText + '</a> \
                 </div> \
             </div> \
             <div class="modal-footer"> \
@@ -168,7 +173,8 @@ function composeForm(data) {
     ');
 
     return signinDialog;
-};
+}
+;
 
 /**
  * Show modal dialog.
@@ -178,11 +184,12 @@ function showSigninDialog(data) {
 
     // show dialog
     signinDialog.modal({
-      "backdrop" : "static",
-      "keyboard" : true,
-      "show" : true // this parameter ensures the modal is shown immediately
+        "backdrop":"static",
+        "keyboard":true,
+        "show":true // this parameter ensures the modal is shown immediately
     });
 
     signinDialog.find("#j_username").focus();
     return signinDialog;
-};
+}
+;
