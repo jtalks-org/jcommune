b76fbf3fdc880d63a9afe58e7880cd296da014a6
senleft <alexandreteterin@gmail.com>
2012-11-11 19:34:51 +0400

    #JC-1033 - Available page size adjusted according to JIRA comment http://jira.jtalks.org/browse/JC-1033?focusedCommentId=18403&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-18403

--- jcommune-model/src/main/java/org/jtalks/jcommune/model/entity/JCUser.java
+++ jcommune-model/src/main/java/org/jtalks/jcommune/model/entity/JCUser.java
@@ -44,7 +44,7 @@ public class JCUser extends User {
     public static final int MAX_LOCATION_SIZE = 30;
 
     public static final int DEFAULT_PAGE_SIZE = 50;
-    public static final int[] PAGE_SIZES_AVAILABLE = new int[]{5, 10, 20, 50, 100, 250};
+    public static final int[] PAGE_SIZES_AVAILABLE = new int[]{15, 25, 50};
 
     private static final long serialVersionUID = 19981017L;
     private Set<UserContact> contacts = new HashSet<UserContact>();
