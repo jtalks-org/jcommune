e6c531b6161eee1fe278f6956afe53de7a582bd2
Mikhail Zaitsev <flashed@mail.ru>
2012-11-14 01:55:54 +0400

    JC-910 Fixed Restricted and Allowed problem for registred users

--- jcommune-model/src/main/java/org/jtalks/jcommune/model/entity/ViewTopicsBranches.java
+++ jcommune-model/src/main/java/org/jtalks/jcommune/model/entity/ViewTopicsBranches.java
@@ -25,6 +25,7 @@ public class ViewTopicsBranches extends Entity {
 
     private Long branchId;
     private String sid;
+    private Boolean granting;
 
     /**
      * @return id branch
@@ -56,5 +57,21 @@ public class ViewTopicsBranches extends Entity {
      */
     public void setSid(String sid) {
         this.sid = sid;
+    }
+
+    /**
+     * Get 'Allowed' or 'Restricted' group for branch
+     * @return 1-allowed, 0-restricted
+     */
+    public Boolean getGranting() {
+        return granting;
+    }
+
+    /**
+     * Set 'Allowed' or 'Restricted' group for branch
+     * @param granting 1-allowed, 0-restricted
+     */
+    public void setGranting(Boolean granting) {
+        this.granting = granting;
     }
 }
