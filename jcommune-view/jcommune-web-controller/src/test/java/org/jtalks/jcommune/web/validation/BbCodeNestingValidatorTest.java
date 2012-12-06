81aed08cc9697c618352c07e826bbb2cdf001ba4
Mikhail Zaitsev <flashed@mail.ru>
2012-12-07 00:14:53 +0400

    JC-1134 Changed after review

--- jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/BbCodeNestingValidatorTest.java
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/validation/BbCodeNestingValidatorTest.java
@@ -79,17 +79,16 @@ public class BbCodeNestingValidatorTest {
                 {"[b][/b][b][/b][u][/u][u]text[/u][u][u][u][/u][/u][/u]text"},
                 {"[u][/u]"},
                 {"text"},
-                {"[*][*][*][*][*][*][*][*][*][*][*][*][*][*][*]"}
+                {"[*][*][*][*][*][*][*][*][*][*][*][*][*][*][*]"},
+                {repeat("[b]",50)},
         };
     }
 
     @DataProvider
     public String[][] tooDeepNestingMessages() {
         return new String[][]{ // {"message"}
-                {"[color][b][color][b][b][b][b][b][b][color][b][b][b][b][b][b][b][b][b][b][color][b][b][b][b][b][b][b][b][b][b][b]"+
-                        "[b][b][color][b][b][b][b][b][b][b][b][color][b][color][b][color][color][b][b][b][b][/b][/b][/b][u]"},
-                {"[b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][color][b][b][b][b][b][b][b][b][b][b][b]"+
-                 "[b][b][color][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][b][color][b][/b][/b][/b][u]"}
+                {repeat("[b]",51)},
+                {repeat("[color][u]",50)}
         };
     }
 }
