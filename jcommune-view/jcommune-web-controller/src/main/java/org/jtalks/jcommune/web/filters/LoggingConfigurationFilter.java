669888ef827582f097724095060cdfaf3c778a66
shogun <sshogunn@gmail.com>
2013-01-20 20:31:52 +0600

    #JC-1210 Filter to configure logging was added.

---
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/filters/LoggingConfigurationFilter.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.jtalks.jcommune.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.jtalks.common.security.SecurityService;
+import org.slf4j.MDC;
+
+/**
+ * This filter provides an ability to register
+ * 
+ * @author Anuar_Nurmakanov
+ *
+ */
+public class LoggingConfigurationFilter implements Filter {
+    private final String USER_NAME_KEY = "userName";
+    
+    private SecurityService securityService;
+    
+    /**
+     * Constructs an instance with required fields.
+     * 
+     * @param securityService to get current user for registration in logging context
+     */
+    public LoggingConfigurationFilter(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //empty
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String currentUserName = securityService.getCurrentUserUsername();
+        boolean successfulRegistered = registerCurrentUserName(currentUserName);
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            if (successfulRegistered) {
+                MDC.remove(USER_NAME_KEY);
+            }
+        }
+    }
+    
+    /**
+     * Register the user in the MDC under USER_KEY.
+     * 
+     * @param userName the name of current user
+     * @return true id the user can be successfully registered
+     */
+    private boolean registerCurrentUserName(String userName) {
+      if (!StringUtils.isEmpty(userName)) {
+          MDC.put(USER_NAME_KEY, userName);
+          return true;
+      }
+      return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy() {
+      //empty  
+    }
+}
