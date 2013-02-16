90dd78138237cd377aef8dafdfa18afc1a44c0da
senleft <alexandreteterin@gmail.com>
2013-02-16 19:06:25 +0400

    #JC-1251 Added service  tests.

---
+++ jcommune-service/src/test/java/org/jtalks/jcommune/service/transactional/TransactionalLinkServiceTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.jtalks.jcommune.service.transactional;
+
+import org.jtalks.jcommune.model.dao.ExternalLinkDao;
+import org.jtalks.jcommune.model.entity.ExternalLink;
+import org.jtalks.jcommune.service.ExternalLinkService;
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * @author Alexandre Teterin
+ *         Date: 16.02.13
+ */
+
+
+public class TransactionalLinkServiceTest {
+
+    @Mock
+    private ExternalLinkDao dao;
+    private ExternalLinkService service;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initMocks(this);
+        service = new TransactionalLinkService(dao);
+    }
+
+    @Test
+    public void testGetLinks() throws Exception {
+        service.getLinks();
+        verify(dao).getAll();
+    }
+
+    @Test
+    public void testAddLink() throws Exception {
+        service.addLink(new ExternalLink());
+        verify(dao).saveOrUpdate(any(ExternalLink.class));
+    }
+
+    @Test
+    public void testRemoveLink() throws Exception {
+        service.removeLink(1L);
+        verify(dao).delete(eq(1L));
+    }
+}
