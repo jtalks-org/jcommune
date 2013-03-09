4fc181147d33e43b9fbf43bcf5541ac291d16f3a
lamao <lamao@yandex.ru>
2013-03-09 10:04:43 +0200

    #JC-1326. Last read post after post deleting.

--- jcommune-service/src/main/java/org/jtalks/jcommune/service/transactional/TransactionalPostService.java
+++ jcommune-service/src/main/java/org/jtalks/jcommune/service/transactional/TransactionalPostService.java
@@ -117,6 +117,8 @@ public class TransactionalPostService extends AbstractTransactionalEntityService
                   "(hasPermission(#post.topic.branch.id, 'BRANCH', 'BranchPermission.DELETE_OTHERS_POSTS') and " +
                   "#post.userCreated.username != principal.username)")
     public void deletePost(Post post) {
+        lastReadPostService.updateLastReadPostsWhenPostIsDeleted(post);
+        
         JCUser user = post.getUserCreated();
         user.setPostCount(user.getPostCount() - 1);
         Topic topic = post.getTopic();
@@ -131,7 +133,6 @@ public class TransactionalPostService extends AbstractTransactionalEntityService
         topicDao.update(topic);
         securityService.deleteFromAcl(post);
         notificationService.topicChanged(topic);
-        lastReadPostService.updateLastReadPostsWhenPostIsDeleted(post);
         if (deletedPostIsLastPostInBranch) {
             branchLastPostService.refreshLastPostInBranch(branch);
         }
