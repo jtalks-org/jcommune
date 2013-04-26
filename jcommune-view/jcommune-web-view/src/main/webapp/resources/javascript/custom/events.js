6704b83b22925547a665542079e9d7ceb7048c78
istsergey <istsergey@gmail.com>
2013-04-26 22:10:22 +0300

    #JC-1401 Added the description in events.js

--- jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/custom/events.js
+++ jcommune-view/jcommune-web-view/src/main/webapp/resources/javascript/custom/events.js
@@ -14,13 +14,23 @@
  */
 
 /*
-* This script contains custom events
+* This script contains custom JQuery events
 */
 (function ($) {
+    /**
+     * valuechange
+     *
+     * This event fires whenever the value changes in text field
+     *
+     * Example of use:
+     *      $("#text-field").bind('valuechange', function() {
+	 *          //your code
+	 *      });
+     */
     $.event.special.valuechange = {
         setup: function () {
-            $(this).bind('keyup.valuechange', $.event.special.valuechange.handler);
-            $(this).bind('cut.valuechange input.valuechange', $.event.special.valuechange.delayedHandler);
+            $(this).bind('keyup.valuechange input.valuechange', $.event.special.valuechange.handler);
+            $(this).bind('cut.valuechange', $.event.special.valuechange.delayedHandler);
             // for IE
             $(this).bind('propertychange.valuechange', $.event.special.valuechange.ieHandler);
         },
@@ -39,6 +49,7 @@
         },
         delayedHandler: function () {
             var element = $(this);
+            //setTimeout need because the cut event fires before the selected data is removed
             setTimeout(function () {
                 $.event.special.valuechange.triggerEvent(element);
             }, 10);
