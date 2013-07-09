12d2fa3cd908a153f3828350d5bfe52664e7967d
sshogunn <sshogunn@gmail.com>
2013-07-10 00:00:19 +0600

    #JC-1512 First version of model.

---
+++ jcommune-model/src/main/java/org/jtalks/jcommune/model/entity/Plugin.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.jtalks.jcommune.model.entity;
+
+import org.jtalks.common.model.entity.Entity;
+
+import java.util.List;
+
+/**
+ *
+ * @author Anuar Nurmakanov
+ */
+public class Plugin extends Entity {
+    private String name;
+    private boolean isActive;
+    private List<PluginProperty> properties;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public List<PluginProperty> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<PluginProperty> properties) {
+        this.properties = properties;
+    }
+}
