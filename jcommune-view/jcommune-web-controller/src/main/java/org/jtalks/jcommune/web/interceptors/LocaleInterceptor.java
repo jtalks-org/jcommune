4006868339f94ed45011fba57b4cb1e261fe36d6
Spazon <beltesting@gmail.com>
2013-08-14 22:43:45 +0200

    LocaleInterceptor file added.

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/interceptors/LocaleInterceptor.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/interceptors/LocaleInterceptor.java
@@ -5,29 +5,50 @@ import org.jtalks.jcommune.model.entity.Language;
 import org.jtalks.jcommune.service.UserService;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 public class LocaleInterceptor extends LocaleChangeInterceptor {
-    private String paramName = DEFAULT_PARAM_NAME;
     UserService userService;
 
     public LocaleInterceptor(UserService userService) {
-        super();
-        super.setParamName(paramName);
         this.userService=userService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException {
+        super.preHandle(request, response, handler);
+        JCUser jcuser = userService.getCurrentUser();
+        if(!jcuser.isAnonymous()){
+            try{
+            jcuser.setLanguage(Language.valueOf(request.getParameter(getParamName())));
+            }catch (Exception e){
+                throw new ServletException("Languale save failed.");
+            }
+        }
+        return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         super.afterCompletion(request, response, handler, ex);
         JCUser jcuser = userService.getCurrentUser();
-        if(!jcuser.isAnonymous()){
-            try{
-            jcuser.setLanguage(Language.valueOf(request.getParameter(paramName)));
-            }catch (Exception e){
-                throw new Exception("Languale save failed.");
-            }
+        Language currentLanguage = jcuser.getLanguage();
+        Language expectedLanguage =(Language)Language.valueOf(response.getLocale().toString());
+        try{
+        expectedLanguage =Language.valueOf((String)request.getAttribute(getParamName()));
+        }catch (NullPointerException npe){
+            expectedLanguage = null;
+        }
+
+        try{
+        if(!currentLanguage.equals(expectedLanguage) && !jcuser.isAnonymous()){
+            jcuser.setLanguage(expectedLanguage);
+        }
+        }catch (Exception e){
+            throw new ServletException("SETTING LANGUAGE FAILED");
         }
     }
 }
