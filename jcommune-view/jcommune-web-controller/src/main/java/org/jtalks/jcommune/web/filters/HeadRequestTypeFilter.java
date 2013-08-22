32247aaf5e6e049230f5249a72ca7b6fd3899272
senleft <alexandreteterin@gmail.com>
2013-08-22 18:33:14 +0400

    #JC-1504 Added licence header.

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/filters/HeadRequestTypeFilter.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/filters/HeadRequestTypeFilter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.jtalks.jcommune.web.filters;
 
 import org.jtalks.jcommune.web.filters.wrapper.ForceGetRequestWrapper;
@@ -5,7 +19,6 @@ import org.jtalks.jcommune.web.filters.wrapper.NoBodyResponseWrapper;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
