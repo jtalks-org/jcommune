cf83f493dad5e6b029fffea0f43f2cb3a013a289
sbashkyrtsev <sbashkyrtsev@griddynamics.com>
2013-09-20 15:01:09 +0400

    * Lowered the level of logging about mentioned users in posts to trace. * Additionally got rid of warnings in the class.

--- jcommune-service/src/main/java/org/jtalks/jcommune/service/nontransactional/MentionedUsers.java
+++ jcommune-service/src/main/java/org/jtalks/jcommune/service/nontransactional/MentionedUsers.java
@@ -22,7 +22,6 @@ import org.jtalks.jcommune.model.entity.JCUser;
 import org.jtalks.jcommune.model.entity.Post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.util.UriUtils;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -31,7 +30,6 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -43,29 +41,24 @@ import static java.lang.String.format;
  * This service provides send email notifications to all mentioned users
  * in some components of forum:topics, posts. Also it provides an ability
  * to extract users mentioning from text.
- * 
+ *
  * @author Anuar_Nurmakanov
  * @author Andrei Alikov
- *
  */
 public class MentionedUsers {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MentionedUsers.class);
-
-    private static final Pattern ALL_MENTIONED_USERS_PATTERN = 
-            Pattern.compile("\\[user\\].*?\\[/user\\]|\\[user notified=true\\].*?\\[/user\\]");
-    private static final Pattern MENTIONED_AND_NOT_NOTIFIED_USERS_PATTERN = 
-            Pattern.compile("\\[user\\].*?\\[/user\\]");
     public static final String MENTIONED_NOT_NOTIFIED_USER_TEMPLATE = "[user]%s[/user]";
     public static final String MENTIONED_AND_NOTIFIED_USER_TEMPLATE = "[user notified=true]%s[/user]";
     public static final String USER_WITH_LINK_TO_PROFILE_TEMPLATE = "[user=%s]%s[/user]";
-
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MentionedUsers.class);
+    private static final Pattern ALL_MENTIONED_USERS_PATTERN =
+            Pattern.compile("\\[user\\].*?\\[/user\\]|\\[user notified=true\\].*?\\[/user\\]");
+    private static final Pattern MENTIONED_AND_NOT_NOTIFIED_USERS_PATTERN =
+            Pattern.compile("\\[user\\].*?\\[/user\\]");
     private static final String CLOSE_BRACKET_CODE_PLACEHOLDER = "@w0956756wo@";
     private static final String OPEN_BRACKET_CODE_PLACEHOLDER = "@ywdffgg434y@";
     private static final String SLASH_CODE_PLACEHOLDER = "14@123435vggv4f";
     private static final String LOWER_THEN_PLACEHOLDER = "gertfgertgf@@@@@#4324234";
-
-    private static final Map<String, String> CHARS_PLACEHOLDERS = new HashMap<String, String>();
+    private static final Map<String, String> CHARS_PLACEHOLDERS = new HashMap<>();
 
     static {
         CHARS_PLACEHOLDERS.put("[", OPEN_BRACKET_CODE_PLACEHOLDER);
@@ -74,13 +67,11 @@ public class MentionedUsers {
         CHARS_PLACEHOLDERS.put("<", LOWER_THEN_PLACEHOLDER);
     }
 
-    private final Map<String, String> encodedUserNames = new HashMap<String, String>();
-
+    private final Map<String, String> encodedUserNames = new HashMap<>();
     /**
      * Content of the post
      */
     private String postContent;
-
     /**
      * Post with mentioned users
      */
@@ -115,6 +106,7 @@ public class MentionedUsers {
 
     /**
      * Get list of the users which have to receive notification
+     *
      * @param userDao service for user related operations
      * @return list of the users which should be notified that they were mentioned
      * @throws IllegalStateException when instance was not created based on Post object
@@ -129,11 +121,12 @@ public class MentionedUsers {
             return getNewUsersToNotify(mentionedUsersNames, userDao);
         }
 
-        return Collections.EMPTY_LIST;
+        return new ArrayList<>();
     }
 
     /**
      * Marks all users in user BB codes as already notified
+     *
      * @param postDao service for post related operations
      * @throws IllegalStateException when instance was not created based on Post object
      */
@@ -150,66 +143,59 @@ public class MentionedUsers {
 
     /**
      * Returns post text with BB codes replaced by user profile links
+     *
      * @param userDao service for working with user objects
      * @return text with BB codes replaced by user profile links
      */
     public String getTextWithProcessedUserTags(UserDao userDao) {
         Set<String> mentionedUsers = extractAllMentionedUsers(postContent);
-        Map<String, String> userToUserProfileLinkMap = new HashMap<String, String>();
-        for (String mentionedUser: mentionedUsers) {
+        Map<String, String> userToUserProfileLinkMap = new HashMap<>();
+        for (String mentionedUser : mentionedUsers) {
             String mentionedUserProfileLink = getLinkToUserProfile(mentionedUser, userDao);
             userToUserProfileLinkMap.put(mentionedUser, mentionedUserProfileLink);
         }
         return addLinksToUserProfileForMentionedUsers(postContent, userToUserProfileLinkMap);
     }
 
-
-
     /**
      * Extract names of all users that were mentioned in passed text.
-     * 
+     *
      * @return extracted users' names
      */
     public Set<String> extractAllMentionedUsers(String canContainMentionedUsers) {
         return extractMentionedUsers(canContainMentionedUsers, ALL_MENTIONED_USERS_PATTERN);
     }
 
-    
     /**
      * Extract names of users that were mentioned but not notified yet
-     * 
+     *
      * @return names of users that were mentioned but not notified yet
      */
     private Set<String> extractNotNotifiedMentionedUsers(String canContainMentionedUsers) {
         return extractMentionedUsers(canContainMentionedUsers, MENTIONED_AND_NOT_NOTIFIED_USERS_PATTERN);
     }
-    
+
     /**
      * Extract names of users that were mentioned in passed text.
-     * 
+     *
      * @param canContainMentionedUsers can contain users mentioning
-     * @param mentionedUserPattern pattern to extract mentioned user in given text
+     * @param mentionedUserPattern     pattern to extract mentioned user in given text
      * @return extracted users' names
      */
     private Set<String> extractMentionedUsers(String canContainMentionedUsers, Pattern mentionedUserPattern) {
         if (!StringUtils.isEmpty(canContainMentionedUsers)) {
             Matcher matcher = mentionedUserPattern.matcher(canContainMentionedUsers);
-            Set<String> mentionedUsernames = new HashSet<String>();
+            Set<String> mentionedUsernames = new HashSet<>();
             while (matcher.find()) {
                 String userBBCode = matcher.group();
                 String mentionedUser = userBBCode.replaceAll("\\[.*?\\]", StringUtils.EMPTY);
                 mentionedUsernames.add(decodeUsername(mentionedUser));
             }
             return mentionedUsernames;
-        } 
+        }
         return Collections.emptySet();
     }
 
-    /**
-     *
-     * @param encodedUsername
-     * @return
-     */
     private String decodeUsername(String encodedUsername) {
         String decodeUserName = encodedUsername;
 
@@ -221,25 +207,19 @@ public class MentionedUsers {
         for (Map.Entry<String, String> decodeEntry : CHARS_PLACEHOLDERS.entrySet()) {
             decodeUserName = decodeUserName.replace(decodeEntry.getValue(), decodeEntry.getKey());
         }
-
         encodedUserNames.put(decodeUserName, encodedUsername);
-
         return decodeUserName;
     }
 
-    /**
-     *
-     * @param decodedUsername
-     * @return
-     */
     private String encodeUsername(String decodedUsername) {
         return encodedUserNames.get(decodedUsername);
     }
 
     /**
      * Invokes JavaScript function via built-in JavaScript engine
+     *
      * @param functionName name of the function to be invoked
-     * @param arguments arguments of the function joined in one string
+     * @param arguments    arguments of the function joined in one string
      * @return result of the invocation or null if some error happened
      */
     private Object invokeJavaScript(String functionName, String arguments) {
@@ -264,15 +244,16 @@ public class MentionedUsers {
 
     /**
      * Gets list of users which should be notified
+     *
      * @param mentionedUsernames the set of names of mentioned users
-     * @param userDao service for working with JCUser objects
+     * @param userDao            service for working with JCUser objects
      * @return list of users which should be notified
      */
     private List<JCUser> getNewUsersToNotify(Set<String> mentionedUsernames, UserDao userDao) {
         List<JCUser> mentionedUsers = userDao.getByUsernames(mentionedUsernames);
-        List<JCUser> usersToNotify = new ArrayList<JCUser>();
+        List<JCUser> usersToNotify = new ArrayList<>();
 
-        for (JCUser mentionedUser: mentionedUsers) {
+        for (JCUser mentionedUser : mentionedUsers) {
             if (shouldNotificationBeSent(mentionedUser)) {
                 usersToNotify.add(mentionedUser);
             }
@@ -283,44 +264,41 @@ public class MentionedUsers {
 
     /**
      * Determines if it is needed to send notification to the user
+     *
      * @param mentionedUser this user was mentioned
      * @return true if we need to send notification and false otherwise
      */
     private boolean shouldNotificationBeSent(JCUser mentionedUser) {
         boolean isOtherNotificationAlreadySent = post.getTopicSubscribers().contains(mentionedUser);
-        if (!isOtherNotificationAlreadySent && mentionedUser.isMentioningNotificationsEnabled()) {
-            return true;
-        }
-
-        return false;
+        return !isOtherNotificationAlreadySent && mentionedUser.isMentioningNotificationsEnabled();
     }
-    
+
     /**
      * Mark user tags as already notified
-     * 
+     *
      * @param mentionedUsernames the set of names of mentioned users
-     * @param postDao service for working with Post objects
+     * @param postDao            service for working with Post objects
      */
     private void markUsersAsAlreadyNotified(Set<String> mentionedUsernames, PostDao postDao) {
-        for (String user: mentionedUsernames) {
+        for (String user : mentionedUsernames) {
             markUserAsAlreadyNotified(user, postDao);
         }
     }
 
     /**
      * Change BB user tag to mark user as already notified
-     * 
+     *
      * @param username this user was mentioned
-     * @param postDao service for working with Post objects
+     * @param postDao  service for working with Post objects
      */
     private void markUserAsAlreadyNotified(String username, PostDao postDao) {
-            String initialUserMentioning = format(MENTIONED_NOT_NOTIFIED_USER_TEMPLATE, username);
-            String notifiedUserMentioning = format(MENTIONED_AND_NOTIFIED_USER_TEMPLATE, username);
+        String initialUserMentioning = format(MENTIONED_NOT_NOTIFIED_USER_TEMPLATE, username);
+        String notifiedUserMentioning = format(MENTIONED_AND_NOTIFIED_USER_TEMPLATE, username);
 
-            String newPostContent =
-                    post.getPostContent().replace(initialUserMentioning, notifiedUserMentioning);
-            post.setPostContent(newPostContent);
-            postDao.saveOrUpdate(post);
+        String newPostContent =
+                post.getPostContent().replace(initialUserMentioning, notifiedUserMentioning);
+        post.setPostContent(newPostContent);
+        postDao.saveOrUpdate(post);
     }
 
     /**
@@ -335,10 +313,9 @@ public class MentionedUsers {
         JCUser user = userDao.getByUsername(username);
         if (user != null && user.getUsername().equals(username)) {
             userPofileLink = getApplicationNameAsContextPath() + "/users/" + user.getId();
-            LOGGER.debug("{} has the following url of profile - {}" , username, userPofileLink);
-        }
-        else {
-            LOGGER.debug("Mentioned user wasn't find: {}", username);
+            LOGGER.trace("{} has the following url of profile - {}", username, userPofileLink);
+        } else {
+            LOGGER.trace("Mentioned user wasn't find: {}", username);
         }
 
         return userPofileLink;
@@ -358,14 +335,14 @@ public class MentionedUsers {
     /**
      * Add links to users' profiles for mentioned users.
      *
-     * @param source will be changed and all mentioned users in it will contain links to their profiles
+     * @param source                   will be changed and all mentioned users in it will contain links to their profiles
      * @param userToUserProfileLinkMap user to it links of profile map
      * @return source with users with attached links to profiles
      */
     private String addLinksToUserProfileForMentionedUsers(
             String source, Map<String, String> userToUserProfileLinkMap) {
         String changedSource = source;
-        for (Map.Entry<String, String> userToLinkMap: userToUserProfileLinkMap.entrySet()) {
+        for (Map.Entry<String, String> userToLinkMap : userToUserProfileLinkMap.entrySet()) {
             String username = encodeUsername(userToLinkMap.getKey());
             String userNotNotifiedBBCode = format(MENTIONED_NOT_NOTIFIED_USER_TEMPLATE, username);
             String userNotifiedBBCode = format(MENTIONED_AND_NOTIFIED_USER_TEMPLATE, username);
