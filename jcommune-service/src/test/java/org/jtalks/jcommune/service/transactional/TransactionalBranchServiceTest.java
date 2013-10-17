f2e2bcd26076cde73ff44f07b35b613c6d7a2434
Guram Savinov <guram.savinov@gmail.com>
2013-10-17 19:48:28 +0400

    #JC-916 Changed tests for BranchService#getAvailableBranchesInSection method.

--- jcommune-service/src/test/java/org/jtalks/jcommune/service/transactional/TransactionalBranchServiceTest.java
+++ jcommune-service/src/test/java/org/jtalks/jcommune/service/transactional/TransactionalBranchServiceTest.java
@@ -16,6 +16,7 @@ package org.jtalks.jcommune.service.transactional;
 
 import org.jtalks.common.model.entity.Group;
 import org.jtalks.common.model.entity.Section;
+import org.jtalks.common.model.permissions.BranchPermission;
 import org.jtalks.jcommune.model.dao.BranchDao;
 import org.jtalks.jcommune.model.dao.SectionDao;
 import org.jtalks.jcommune.model.dao.TopicDao;
@@ -25,20 +26,19 @@ import org.jtalks.jcommune.service.TopicModificationService;
 import org.jtalks.jcommune.service.UserService;
 import org.jtalks.jcommune.service.exceptions.NotFoundException;
 import org.jtalks.jcommune.service.security.PermissionService;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.validation.ValidationException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 /**
@@ -101,7 +101,7 @@ public class TransactionalBranchServiceTest {
     }
 
     @Test
-    public void testGetBranchesInSection() throws NotFoundException {
+    public void getBranchesInSectionShouldRemoveTopicBranch() throws NotFoundException {
         JCUser user = new JCUser("username", "email", "password");
         user.getGroups().add(new Group("Registered Users"));
         List<Branch> branches = createBranchList(user);
@@ -109,20 +109,21 @@ public class TransactionalBranchServiceTest {
         Section section = new Section("section");
         for (Branch branch : branches) {
             section.addOrUpdateBranch(branch);
+            when(permissionService.hasBranchPermission(branch.getId(), BranchPermission.VIEW_TOPICS)).thenReturn(true);
         }
 
+        Topic topic = branches.get(0).getTopics().get(0);
         when(userService.getCurrentUser()).thenReturn(user);
         when(sectionDao.isExist(SECTION_ID)).thenReturn(true);
         when(sectionDao.get(SECTION_ID)).thenReturn(section);
-        when(branchDao.getAllAvailableBranchesInSection(user, section)).thenReturn(branches);
-        when(topicDao.get(TOPIC_ID)).thenReturn(branches.get(0).getTopics().get(0));
+        when(topicDao.get(TOPIC_ID)).thenReturn(topic);
 
         List<Branch> result = branchService.getAvailableBranchesInSection(SECTION_ID, TOPIC_ID);
-        assertEquals(result.size(), branches.size() - 1, "Topic shouldn't be accessible for move to the same branch.");
+        assertFalse(result.contains(topic.getBranch()), "Topic shouldn't be accessible for move to the same branch.");
     }
 
     @Test
-    public void testGetBranchesInSectionUserHasNotAnyPermissions() throws NotFoundException {
+    public void getBranchesInSectionCheckPermission() throws NotFoundException {
         JCUser user = new JCUser("username", "email", "password");
         List<Branch> branches = createBranchList(user);
 
@@ -131,16 +132,21 @@ public class TransactionalBranchServiceTest {
             section.addOrUpdateBranch(branch);
         }
 
+        Topic topic = branches.get(0).getTopics().get(0);
+        when(permissionService.hasBranchPermission(branches.get(1).getId(),
+                BranchPermission.VIEW_TOPICS)).thenReturn(true);
         when(userService.getCurrentUser()).thenReturn(user);
         when(sectionDao.isExist(SECTION_ID)).thenReturn(true);
+        when(sectionDao.get(SECTION_ID)).thenReturn(section);
+        when(topicDao.get(TOPIC_ID)).thenReturn(topic);
 
         List<Branch> result = branchService.getAvailableBranchesInSection(SECTION_ID, TOPIC_ID);
-        assertEquals(result.size(), 0, "User shouldn't see any branch without permissions.");
+        assertEquals(result.size(), 1, "User shouldn't see branches without view permission.");
     }
 
     @Test(expectedExceptions = {NotFoundException.class})
-    public void testGetBranchesInSectionFail() throws NotFoundException {
-        when(sectionDao.isExist(Matchers.<Long>any())).thenReturn(false);
+    public void getBranchesInSectionWithWrongSectionId() throws NotFoundException {
+        when(sectionDao.isExist(SECTION_ID)).thenReturn(false);
         branchService.getAvailableBranchesInSection(SECTION_ID, TOPIC_ID);
     }
 
