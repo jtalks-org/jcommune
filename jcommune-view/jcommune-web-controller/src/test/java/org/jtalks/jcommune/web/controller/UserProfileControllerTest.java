986f43204ad718f2477a211789209b1679ec0395
masyan <jmasyan@gmail.com>
2013-10-19 00:37:25 +0600

    #JC-1629 fix moderators view in section

--- jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/controller/UserProfileControllerTest.java
+++ jcommune-view/jcommune-web-controller/src/test/java/org/jtalks/jcommune/web/controller/UserProfileControllerTest.java
@@ -172,7 +172,7 @@ public class UserProfileControllerTest {
         profileController.startEditUserProfile(editedUserId);
     }
 
-    @Test(enabled = false)
+    @Test
     public void saveEditedProfileWithCorrectEnteredDataShouldMoveUserInUpdatedProfile() throws NotFoundException {
         JCUser user = getUser();
         EditUserProfileDto userDto = getEditUserProfileDto();
