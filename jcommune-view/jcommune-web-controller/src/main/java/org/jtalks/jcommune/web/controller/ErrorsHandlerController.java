2b17369bbef7ed1967eb14bc032330c5e3881ebf
Andrey Ivanov <a.nigredo@gmail.com>
2013-11-21 13:59:01 +0200

    #JC-1659: * Added controller for catch 500 error and set correct http status.

---
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/ErrorsHandlerController.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.jtalks.jcommune.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * This controller is needed to handle errors and set correct status
+ *
+ * @author Andrey Ivanov
+ */
+@Controller
+@RequestMapping("/errors/")
+public class ErrorsHandlerController {
+
+    @RequestMapping(value = "500")
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleInternalServerError() {
+        return "/errors/500";
+    }
+}
