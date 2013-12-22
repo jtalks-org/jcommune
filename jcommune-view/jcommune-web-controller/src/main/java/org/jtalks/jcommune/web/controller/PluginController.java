12ab04ac093d3b7b73d7000803e33f9b2c726a1b
ctapobep <ctapobep@javatalks.ru>
2013-12-22 10:52:34 +0400

    #JC-1675 Removed unused imports in PluginController

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/PluginController.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/controller/PluginController.java
@@ -16,7 +16,6 @@ package org.jtalks.jcommune.web.controller;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.jtalks.common.service.exceptions.NotFoundException;
-import org.jtalks.jcommune.model.entity.Language;
 import org.jtalks.jcommune.model.entity.PluginConfiguration;
 import org.jtalks.jcommune.model.entity.PluginProperty;
 import org.jtalks.jcommune.model.plugins.Plugin;
@@ -30,11 +29,16 @@ import org.jtalks.jcommune.service.plugins.PluginLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Provides an ability to manage plugins of the forum.
