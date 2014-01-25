633bb9a5dd4d3482e68f3c643e3f0d27d6e16bb3
Temdegon <willcode4food@tut.by>
2014-01-25 21:55:51 +0300

    JC-1753  minor changes

---
+++ jcommune-model/src/main/java/org/jtalks/hibernate4/spring/fix/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/**
+ * Package contains fixed spring classes to make it working with Hibernate 4.3.
+ * Details can be found here
+ * https://jira.springsource.org/browse/SPR-10839
+ * http://stackoverflow.com/questions/16417217/transactionmanager-cannot-initialize/18255242#18255242
+ * The package should be removed with the Spring version will be updated.
+ *
+ */
+package org.jtalks.hibernate4.spring.fix;
