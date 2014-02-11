864fe326173c5bac30ae6473242abdf681dabd7e
Mikhail Stryzhonok <mixas.27@yandex.ru>
2014-02-11 20:30:11 +0300

    #JC-1743 overriden processAutoLoginCookie method for possibility store tokens in cache and print more info into log

--- jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/rememberme/RememberMeServices.java
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/rememberme/RememberMeServices.java
@@ -14,12 +14,22 @@
  */
 package org.jtalks.jcommune.web.rememberme;
 
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.rememberme.CookieTheftException;
+import org.springframework.security.web.authentication.rememberme.InvalidCookieException;
+import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationException;
 
 /**
  * Implements our custom Remember Me service to replace the Spring default one. This implementation removes Remember Me
@@ -31,8 +41,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RememberMeServices extends PersistentTokenBasedRememberMeServices {
     private final static String REMOVE_TOKEN_QUERY = "DELETE FROM persistent_logins WHERE series = ? AND token = ?";
+    private final static int TOCKEN_CACHE_MAX_SIZE = 500;
     private final RememberMeCookieDecoder rememberMeCookieDecoder;
     private final JdbcTemplate jdbcTemplate;
+    private final Queue<PersistentRememberMeToken> tokenCache = new ConcurrentLinkedQueue<>();
+    private PersistentTokenRepository tokenRepository;
 
     /**
      * @param rememberMeCookieDecoder needed for extracting rememberme cookies
@@ -61,6 +74,97 @@ public class RememberMeServices extends PersistentTokenBasedRememberMeServices {
             }
             cancelCookie(request, response);
             jdbcTemplate.update(REMOVE_TOKEN_QUERY, seriesAndToken);
+            removeTokenFromCache(seriesAndToken);
+        }
+    }
+    
+    /**
+     * Temporary solution to localize "remember-me" authentication bug. 
+     * Recently used tokens stores in cache. If CookieTheftException was thrown, performed search of tokens in cache. 
+     * {@inheritDoc}
+     */
+    @Override
+    protected UserDetails processAutoLoginCookie(String[] cookieTokens, HttpServletRequest request, HttpServletResponse response) {
+         if (cookieTokens.length != 2) {
+            throw new InvalidCookieException("Cookie token did not contain " + 2 +
+                    " tokens, but contained '" + Arrays.asList(cookieTokens) + "'");
+        }
+         
+        final String presentedSeries = cookieTokens[0];
+        final PersistentRememberMeToken token = tokenRepository.getTokenForSeries(presentedSeries);
+        
+         if (token == null) {
+            throw new RememberMeAuthenticationException("No persistent token found for series id: " + presentedSeries);
+        }
+        cacheToken(token);
+        
+        try {
+            final UserDetails userDetails = super.processAutoLoginCookie(cookieTokens, request, response); 
+            return userDetails;
+            
+        } catch (CookieTheftException cte) {
+            
+            logger.debug("Search for token in token cache");
+            
+            final PersistentRememberMeToken cachedTocken = findTockenInCache(cookieTokens);
+            if (cachedTocken == null) {
+                if (logger.isDebugEnabled()) {
+                logger.debug("Token with series '" + cachedTocken.getSeries() + "' and value '"
+                        + cachedTocken.getTokenValue() + "' not found in cache");
+                }
+                throw cte;
+            }
+            
+            if (logger.isDebugEnabled()) {
+                logger.debug("Token for user '" + cachedTocken.getUsername() + "', series '" +
+                    cachedTocken.getSeries() + "' found in cache");
+            }
+            try {
+                tokenRepository.createNewToken(cachedTocken);
+                setCookie(new String[] {cachedTocken.getSeries(), cachedTocken.getTokenValue()}, getTokenValiditySeconds(),
+                        request, response);
+            } catch (DataAccessException e) {
+                 logger.error("Failed to save persistent token ", e);
+            }
+            return getUserDetailsService().loadUserByUsername(cachedTocken.getUsername());
+        }
+    }
+    
+    /**
+    * {@inheritDoc}
+    */
+    @Override
+    public void setTokenRepository(PersistentTokenRepository tokenRepository) {
+        super.setTokenRepository(tokenRepository);
+        this.tokenRepository = tokenRepository;
+    }
+    
+
+    private void cacheToken(PersistentRememberMeToken token) {
+        if (tokenCache.size() == TOCKEN_CACHE_MAX_SIZE) {
+            tokenCache.poll();
+        }
+        tokenCache.add(token);
+    }
+    
+    private PersistentRememberMeToken findTockenInCache(String[] cookieTokens) {
+        final String presentedSeries = cookieTokens[0];
+        final String presentedToken = cookieTokens[1];
+        for (final PersistentRememberMeToken token : tokenCache) {
+            if (presentedToken.equals(token.getTokenValue()) && presentedSeries.equals(token.getSeries())) {
+                return token;
+            }
+        }
+        return null;
+    }
+    
+    private void removeTokenFromCache(String[] seriesAndToken) {
+        final String presentedSeries = seriesAndToken[0];
+        final String presentedToken = seriesAndToken[1];
+        for (final PersistentRememberMeToken token : tokenCache) {
+            if (presentedToken.equals(token.getTokenValue()) && presentedSeries.equals(token.getSeries())) {
+                tokenCache.remove(token);
+            }
         }
     }
 }
