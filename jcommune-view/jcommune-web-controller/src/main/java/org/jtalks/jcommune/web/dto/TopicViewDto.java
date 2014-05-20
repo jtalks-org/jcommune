adfa60c6aed24e12ab62542ede6618aabf1e5d89
Mikhail Stryzhonok <mixas.27@yandex.ru>
2014-05-20 17:12:07 +0300

    #JC-1756: Error reading creation date:       *Added dto for preview topic in branch

---
+++ jcommune-view/jcommune-web-controller/src/main/java/org/jtalks/jcommune/web/dto/TopicViewDto.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.jtalks.jcommune.web.dto;
+
+import org.jtalks.jcommune.model.entity.JCUser;
+import org.jtalks.jcommune.model.entity.Post;
+import org.jtalks.jcommune.model.entity.Topic;
+
+/**
+ * @author  Mikhail Stryzhonok.
+ */
+public class TopicViewDto {
+
+    private long id;
+    private String title;
+    private boolean sticked;
+    private boolean announcement;
+    private boolean hasPoll;
+    private boolean closed;
+    private int views;
+    private int postCount;
+    private boolean hasUpdates;
+    private Long firstUnreadPostId;
+    private JCUser topicStarter;
+    private Post lastPost;
+
+    public TopicViewDto(Topic topic) {
+        this.id = topic.getId();
+        this.title = topic.getTitle();
+        this.sticked = topic.isSticked();
+        this.announcement = topic.isAnnouncement();
+        this.hasPoll = topic.isHasPoll();
+        this.closed = topic.isClosed();
+        this.views = topic.getViews();
+        this.postCount = topic.getPostCount();
+        this.hasUpdates = topic.isHasUpdates();
+        this.firstUnreadPostId = topic.getFirstUnreadPostId();
+        this.topicStarter = topic.getTopicStarter();
+        this.lastPost = topic.getLastPost();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isSticked() {
+        return sticked;
+    }
+
+    public void setSticked(boolean sticked) {
+        this.sticked = sticked;
+    }
+
+    public boolean isAnnouncement() {
+        return announcement;
+    }
+
+    public void setAnnouncement(boolean announcement) {
+        this.announcement = announcement;
+    }
+
+    public boolean isHasPoll() {
+        return hasPoll;
+    }
+
+    public void setHasPoll(boolean hasPoll) {
+        this.hasPoll = hasPoll;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getPostCount() {
+        return postCount;
+    }
+
+    public void setPostCount(int postCount) {
+        this.postCount = postCount;
+    }
+
+    public boolean isHasUpdates() {
+        return hasUpdates;
+    }
+
+    public void setHasUpdates(boolean hasUpdates) {
+        this.hasUpdates = hasUpdates;
+    }
+
+    public Long getFirstUnreadPostId() {
+        return firstUnreadPostId;
+    }
+
+    public void setFirstUnreadPostId(Long firstUnreadPostId) {
+        this.firstUnreadPostId = firstUnreadPostId;
+    }
+
+    public JCUser getTopicStarter() {
+        return topicStarter;
+    }
+
+    public void setTopicStarter(JCUser topicStarter) {
+        this.topicStarter = topicStarter;
+    }
+
+    public Post getLastPost() {
+        return lastPost;
+    }
+
+    public void setLastPost(Post lastPost) {
+        this.lastPost = lastPost;
+    }
+}
