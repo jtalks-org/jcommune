42cad86e585c5a968422ad3685f06eb6a9fbc1b1
Mikhail Stryzhonok <mixas.27@yande.ru>
2014-07-29 16:01:25 +0300

    #JC-1869:Unit tests for StatefullPlugin

---
+++ jcommune-plugin-api/src/test/java/org/jtalks/jcommune/plugin/api/core/StateFullPluginTest.java
@@ -0,0 +1,92 @@
+package org.jtalks.jcommune.plugin.api.core;
+
+import org.jtalks.jcommune.model.entity.PluginConfiguration;
+import org.jtalks.jcommune.model.entity.PluginProperty;
+import org.jtalks.jcommune.plugin.api.exceptions.UnexpectedErrorException;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * @author Mikhail Stryzhonok
+ */
+public class StateFullPluginTest {
+
+    @Mock
+    private PluginConfiguration configuration;
+    @Mock
+    private StatefullPlugin plugin;
+
+    @BeforeMethod
+    public void init() throws Exception {
+        initMocks(this);
+        doCallRealMethod().when(plugin).configure(any(PluginConfiguration.class));
+        doCallRealMethod().when(plugin).getState();
+    }
+
+    @Test
+    public void configureShouldApplyConfiguration() throws Exception{
+        List<PluginProperty> properties = Arrays.asList(new PluginProperty());
+        when(configuration.getProperties()).thenReturn(properties);
+        when(plugin.applyConfiguration(properties)).thenReturn(Collections.<PluginProperty, String>emptyMap());
+
+        plugin.configure(configuration);
+        verify(plugin).applyConfiguration(properties);
+    }
+
+    @Test(expectedExceptions = UnexpectedErrorException.class)
+    public void configureShouldThrowUnexpectedErrorExceptionWhenRuntimeExceptionOccurs() throws Exception{
+        List<PluginProperty> properties = Arrays.asList(new PluginProperty());
+        when(configuration.getProperties()).thenReturn(properties);
+        when(plugin.applyConfiguration(properties)).thenThrow(new RuntimeException());
+
+        plugin.configure(configuration);
+    }
+
+    @Test
+    public void getStateShouldReturnEnabledStateWhenPluginConfiguredAsActive() throws Exception{
+        List<PluginProperty> properties = Arrays.asList(new PluginProperty());
+        when(configuration.getProperties()).thenReturn(properties);
+        when(configuration.isActive()).thenReturn(true);
+        when(plugin.applyConfiguration(properties)).thenReturn(Collections.<PluginProperty, String>emptyMap());
+
+        plugin.configure(configuration);
+
+        assertEquals(plugin.getState(), Plugin.State.ENABLED);
+    }
+
+    @Test
+    public void getStateShouldReturnConfiguredStateWhenPluginConfiguredAsNotActive() throws Exception{
+        List<PluginProperty> properties = Arrays.asList(new PluginProperty());
+        when(configuration.getProperties()).thenReturn(properties);
+        when(configuration.isActive()).thenReturn(false);
+        when(plugin.applyConfiguration(properties)).thenReturn(Collections.<PluginProperty, String>emptyMap());
+
+        plugin.configure(configuration);
+
+        assertEquals(plugin.getState(), Plugin.State.CONFIGURED);
+    }
+
+    @Test
+    public void getStateShouldReturnInErrorStateWhenConfigurationErrorOccurs() {
+        List<PluginProperty> properties = Arrays.asList(new PluginProperty());
+        when(configuration.getProperties()).thenReturn(properties);
+        when(plugin.applyConfiguration(properties)).thenThrow(new RuntimeException());
+
+        try {
+            plugin.configure(configuration);
+        } catch (UnexpectedErrorException ex) {
+        }
+
+        assertEquals(plugin.getState(), Plugin.State.IN_ERROR);
+    }
+}
