55977d1355eda97d55442e1ca396cb538da501fb
Evgeniy Cheban <mister.cheban@gmail.com>
2016-08-14 20:54:49 +0400

    JC-970 Re-indexing is not secured
    - Added componentService in TransactionalTopicFetchService to checking user permissions
    on the route: /search/index/rebuild.
    
    - Added groovy security test which demonstrate that user without admin permissions
    can't send GET request on this route, response status will be 403 and /errors/403 view
    will be displayed.
    
    - Fixed TransactionalTopicFetchServiceTest after made changes in TransactionalTopicFetchService.
    
    - Fixed code format in TopicSearchControllerTest for GitHub.

---
+++ jcommune-view/jcommune-web-view/src/test/java/org/jtalks/jcommune/test/TopicSearchControllerTest.groovy
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2011  JTalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.jtalks.jcommune.test
+
+import org.jtalks.jcommune.test.model.User
+import org.jtalks.jcommune.test.service.ComponentService
+import org.jtalks.jcommune.test.service.GroupsService
+import org.jtalks.jcommune.test.utils.Users
+import org.jtalks.jcommune.web.controller.TopicSearchController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mock.web.MockHttpSession
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.transaction.TransactionConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MvcResult
+import org.springframework.transaction.annotation.Transactional
+import spock.lang.Specification
+
+import javax.servlet.http.HttpSession
+
+import static org.jtalks.jcommune.test.utils.assertions.Assert.assertView
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
+/**
+ * The security test for {@link TopicSearchController}
+ * @author Evgeniy Cheban
+ */
+@WebAppConfiguration
+@ContextConfiguration(locations = 'classpath:/org/jtalks/jcommune/web/view/test-configuration.xml')
+@TransactionConfiguration(transactionManager = 'transactionManager', defaultRollback = true)
+@Transactional
+class TopicSearchControllerTest extends Specification {
+    @Autowired Users users;
+    @Autowired MockMvc mockMvc;
+    @Autowired ComponentService componentService;
+    @Autowired GroupsService groupsService;
+
+    def setup() {
+        groupsService.create();
+    }
+
+    def 'must not be able to send GET request to indexing data from database'() {
+        given: 'User created without admin permissions'
+            def user = new User();
+            users.created(user);
+            def session = users.signIn(user);
+        when: 'send GET request to indexing data from database'
+           MvcResult mvcResult = rebuildIndexes(session);
+        then: 'redirect to 403 error page'
+           assertView(mvcResult, "redirect:/errors/403");
+    }
+
+    def rebuildIndexes(HttpSession session) {
+        componentService.createForumComponent();
+        def result = mockMvc.perform(get('/search/index/rebuild')
+                .session(session as MockHttpSession))
+                .andReturn();
+        return result;
+    }
+}
