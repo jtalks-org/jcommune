349beaa5adc8e3dc6aaa276140c5ba379af07754
Oleg Tkachenko <tk4nko@ukr.net>
2016-08-21 22:15:00 +0300

    JC-2291 Fix column name in 'Group administration" page
    - fixed title for "Name" column (English, Spanish)

--- jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V71__Add_Unique_Key_To_Last_Read_Posts_Table.sql
+++ jcommune-model/src/main/resources/org/jtalks/jcommune/migrations/V71__Add_Unique_Key_To_Last_Read_Posts_Table.sql
@@ -2,11 +2,11 @@
 DELETE FROM LAST_READ_POSTS
 WHERE ID IN (
   SELECT ID FROM (
-    SELECT *
-    FROM LAST_READ_POSTS
-    GROUP BY TOPIC_ID, USER_ID
-    HAVING COUNT(*) > 1
-  ) as tmp
+                   SELECT *
+                   FROM LAST_READ_POSTS
+                   GROUP BY TOPIC_ID, USER_ID
+                   HAVING COUNT(*) > 1
+                 ) as tmp
 );
 
 ALTER TABLE LAST_READ_POSTS ADD UNIQUE INDEX `UNIQUE_TOPIC_ID_USER_ID` (TOPIC_ID, USER_ID);
